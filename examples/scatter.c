ae23f9a12da62b4a267569bb4d67a33a9ecbae9b
Cyrille Rossant <rossant@users.noreply.github.com>
2020-10-12 16:48:43 +0200

    Fix scatter example

--- examples/scatter.c
+++ examples/scatter.c
@@ -61,7 +61,6 @@ int main()
 
     // Set the panel controller.
     VkyAxes2DParams axparams = vky_default_axes_2D_params();
-    axparams.enable_panzoom = false;
 
     // Set the yscale.
     axparams.yscale.vmin = -25;
