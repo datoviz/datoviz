f209642e224f29fe000cfd7c40e0d2f5c2b010a8
Cyrille Rossant <rossant@users.noreply.github.com>
2020-10-14 10:30:36 +0200

    Reorganize axes_3D header

--- src/axes_3D.h
+++ src/axes_3D.h
@@ -4,12 +4,20 @@
 #include "../include/visky/visky.h"
 
 
+/*************************************************************************************************/
+/*  Typedefs                                                                                     */
+/*************************************************************************************************/
+
+typedef struct VkyAxes3DVertex VkyAxes3DVertex;
+typedef struct VkyAxes3DTextParams VkyAxes3DTextParams;
+typedef struct VkyAxes3DTextData VkyAxes3DTextData;
+typedef struct VkyAxes3DTextVertex VkyAxes3DTextVertex;
+
 
 /*************************************************************************************************/
 /*  Axes 3D visual                                                                               */
 /*************************************************************************************************/
 
-typedef struct VkyAxes3DVertex VkyAxes3DVertex;
 struct VkyAxes3DVertex
 {
     float tick;
@@ -20,15 +28,12 @@ struct VkyAxes3DVertex
     uint8_t coord_side; // H: 0x,1y,2z,(3). V: 4x, 5y, 6z, (7)
 };
 
-// VkyVisual* vky_visual_axes_3D(VkyScene* scene);
-
 
 
 /*************************************************************************************************/
 /*  Axes 3D text visual                                                                          */
 /*************************************************************************************************/
 
-typedef struct VkyAxes3DTextParams VkyAxes3DTextParams;
 struct VkyAxes3DTextParams
 {
     ivec2 grid_size;
@@ -37,7 +42,7 @@ struct VkyAxes3DTextParams
     vec4 color;
 };
 
-typedef struct VkyAxes3DTextData VkyAxes3DTextData;
+
 struct VkyAxes3DTextData
 {
     float tick;
@@ -46,15 +51,13 @@ struct VkyAxes3DTextData
     char string[16];
 };
 
-typedef struct VkyAxes3DTextVertex VkyAxes3DTextVertex;
+
 struct VkyAxes3DTextVertex
 {
     float tick;
     uint8_t coord_side; // H: 0x,1y,2z,(3). V: 4x, 5y, 6z, (7)
     usvec4 glyph;       // char, char_index, str_len, str_index
 };
-
-// VkyVisual* vky_visual_axes_3D_text(VkyScene* scene);
 
 
 
