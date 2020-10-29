2f6da17a429d7d4fe137328b080519d526717d4c
Cyrille Rossant <rossant@users.noreply.github.com>
2020-10-29 11:49:49 +0100

    Update build script

--- bindings/cython/build.sh
+++ bindings/cython/build.sh
@@ -1,4 +1,6 @@
 python3 setup.py build_ext --inplace
 python3 setup.py develop --user
 # HACK: need to find a proper way to put the compiled python extension at the right place
-ln -s $(pwd)/_skbuild/*/cmake-build/pyvisky.*.so $(pwd)/visky/
+if ! compgen -G "$(pwd)/visky/pyvisky*.so" > /dev/null ; then
+    ln -s $(pwd)/_skbuild/*/cmake-build/pyvisky.*.so $(pwd)/visky/
+fi
