5888793025bde2ae79578fa62c188afe9e17d1f9
Cyrille Rossant <rossant@users.noreply.github.com>
2020-11-27 09:58:37 +0100

    WIP: function to compile shaders

---
+++ src/spirv.cpp
@@ -0,0 +1,9 @@
+#include "spirv.h"
+#include <SPIRV/spirv.hpp>
+
+
+
+VkShaderModule vkl_shader_compile(const char* glsl_path)
+{ // TODO
+    return (VkShaderModule){0};
+}
---
+++ src/spirv.h
@@ -0,0 +1,13 @@
+#ifndef VKL_SPIRV_HEADER
+#define VKL_SPIRV_HEADER
+
+#include "../include/visky/common.h"
+#include <vulkan/vulkan.h>
+
+
+
+VKY_EXPORT VkShaderModule vkl_shader_compile(const char* glsl_path);
+
+
+
+#endif
