9832b1e44fa27908028228cab4df3149b819f360
Cyrille Rossant <rossant@users.noreply.github.com>
2020-12-11 11:13:51 +0100

    WIP: starting arcball visual

--- cli/test_interact.c
+++ cli/test_interact.c
@@ -7,8 +7,10 @@
 
 
 /*************************************************************************************************/
-/*  Structs                                                                                      */
+/*  Constants                                                                                    */
 /*************************************************************************************************/
+
+#define N_FRAMES 10
 
 
 
@@ -46,7 +48,7 @@ int test_interact_1(TestContext* context)
     vkl_event_callback(canvas, VKL_EVENT_MOUSE_WHEEL, 0, _mouse_callback, &mouse);
     vkl_event_callback(canvas, VKL_EVENT_KEY, 0, _keyboard_callback, &keyboard);
 
-    vkl_app_run(app, 0);
+    vkl_app_run(app, N_FRAMES);
 
     TEST_END
 }
@@ -83,6 +85,45 @@ static void _scene_keyboard_callback(VklCanvas* canvas, VklEvent ev)
     vkl_interact_update(&scene->interact, viewport, &scene->mouse, &scene->keyboard);
 }
 
+static void _add_visual(
+    TestScene* scene, const uint32_t N, VklVertex* vertices, VklGraphicsPointsParams* params)
+{
+    VklVisual* visual = &scene->visual;
+    _marker_visual(visual);
+    VklCanvas* canvas = scene->visual.canvas;
+    VklGpu* gpu = canvas->gpu;
+    VklContext* ctx = gpu->context;
+
+    VklBufferRegions br_viewport = vkl_ctx_buffers(ctx, VKL_DEFAULT_BUFFER_UNIFORM, 1, 16);
+    VklBufferRegions br_params =
+        vkl_ctx_buffers(ctx, VKL_DEFAULT_BUFFER_UNIFORM, 1, sizeof(VklGraphicsPointsParams));
+    VklTexture* tex_color = vkl_ctx_texture(ctx, 2, (uvec3){16, 16, 1}, VK_FORMAT_R8G8B8A8_UNORM);
+
+    vkl_upload_buffers(ctx, br_params, 0, sizeof(VklGraphicsPointsParams), params);
+
+    for (uint32_t i = 0; i < N; i++)
+    {
+        RANDN_POS(vertices[i].pos)
+        RAND_COLOR(vertices[i].color)
+    }
+    vkl_visual_data_buffer(visual, VKL_SOURCE_VERTEX, 0, 0, N, N, vertices);
+    vkl_visual_buffer(visual, VKL_SOURCE_UNIFORM, 1, br_viewport);
+    vkl_visual_buffer(visual, VKL_SOURCE_UNIFORM, 2, br_params);
+    vkl_visual_texture(visual, VKL_SOURCE_TEXTURE_2D, 0, tex_color);
+
+    // MVP.
+    mat4 id = GLM_MAT4_IDENTITY_INIT;
+    vkl_visual_data(visual, VKL_PROP_MODEL, 0, 1, id);
+    vkl_visual_data(visual, VKL_PROP_VIEW, 0, 1, id);
+    vkl_visual_data(visual, VKL_PROP_PROJ, 0, 1, id);
+
+    // Upload the data to the GPU.
+    VklViewport viewport = vkl_viewport_full(canvas);
+    vkl_visual_update(visual, viewport, (VklDataCoords){0}, NULL);
+
+    vkl_canvas_callback(canvas, VKL_PRIVATE_EVENT_REFILL, 0, _visual_canvas_fill, visual);
+}
+
 int test_interact_panzoom(TestContext* context)
 {
     VklApp* app = vkl_app(VKL_BACKEND_GLFW);
@@ -102,47 +143,41 @@ int test_interact_panzoom(TestContext* context)
     vkl_canvas_callback(canvas, VKL_PRIVATE_EVENT_TIMER, 1, _fps, NULL);
 
     const uint32_t N = 10000;
-    mat4 id = GLM_MAT4_IDENTITY_INIT;
     float param = 5.0f;
     VklGraphicsPointsParams params = {.point_size = param};
     VklVertex* vertices = calloc(N, sizeof(VklVertex));
-    {
-        VklVisual* visual = &scene.visual;
-        _marker_visual(visual);
+    _add_visual(&scene, N, vertices, &params);
+    vkl_app_run(app, 0);
 
-        VklContext* ctx = gpu->context;
-        // VklBufferRegions br_mvp =
-        //     vkl_ctx_buffers(ctx, VKL_DEFAULT_BUFFER_UNIFORM_MAPPABLE, 1, sizeof(VklMVP));
-        VklBufferRegions br_viewport = vkl_ctx_buffers(ctx, VKL_DEFAULT_BUFFER_UNIFORM, 1, 16);
-        VklBufferRegions br_params =
-            vkl_ctx_buffers(ctx, VKL_DEFAULT_BUFFER_UNIFORM, 1, sizeof(VklGraphicsPointsParams));
-        VklTexture* tex_color =
-            vkl_ctx_texture(ctx, 2, (uvec3){16, 16, 1}, VK_FORMAT_R8G8B8A8_UNORM);
+    FREE(vertices);
+    TEST_END
+}
 
-        vkl_upload_buffers(ctx, br_params, 0, sizeof(VklGraphicsPointsParams), &params);
 
-        for (uint32_t i = 0; i < N; i++)
-        {
-            RANDN_POS(vertices[i].pos)
-            RAND_COLOR(vertices[i].color)
-        }
-        vkl_visual_data_buffer(visual, VKL_SOURCE_VERTEX, 0, 0, N, N, vertices);
-        vkl_visual_buffer(visual, VKL_SOURCE_UNIFORM, 1, br_viewport);
-        vkl_visual_buffer(visual, VKL_SOURCE_UNIFORM, 2, br_params);
-        vkl_visual_texture(visual, VKL_SOURCE_TEXTURE_2D, 0, tex_color);
 
-        // MVP.
-        vkl_visual_data(visual, VKL_PROP_MODEL, 0, 1, id);
-        vkl_visual_data(visual, VKL_PROP_VIEW, 0, 1, id);
-        vkl_visual_data(visual, VKL_PROP_PROJ, 0, 1, id);
+int test_interact_arcball(TestContext* context)
+{
+    VklApp* app = vkl_app(VKL_BACKEND_GLFW);
+    VklGpu* gpu = vkl_gpu(app, 0);
+    VklCanvas* canvas = vkl_canvas(gpu, TEST_WIDTH, TEST_HEIGHT);
 
-        // Upload the data to the GPU.
-        VklViewport viewport = vkl_viewport_full(canvas);
-        vkl_visual_update(visual, viewport, (VklDataCoords){0}, NULL);
+    TestScene scene = {0};
+    scene.mouse = vkl_mouse();
+    scene.keyboard = vkl_keyboard();
+    scene.interact = vkl_interact_builtin(canvas, VKL_INTERACT_PANZOOM);
+    scene.visual = vkl_visual(canvas);
 
-        vkl_canvas_callback(canvas, VKL_PRIVATE_EVENT_REFILL, 0, _visual_canvas_fill, visual);
-    }
+    vkl_event_callback(canvas, VKL_EVENT_MOUSE_MOVE, 0, _scene_mouse_callback, &scene);
+    vkl_event_callback(canvas, VKL_EVENT_MOUSE_BUTTON, 0, _scene_mouse_callback, &scene);
+    vkl_event_callback(canvas, VKL_EVENT_MOUSE_WHEEL, 0, _scene_mouse_callback, &scene);
+    vkl_event_callback(canvas, VKL_EVENT_KEY, 0, _scene_keyboard_callback, &scene);
+    vkl_canvas_callback(canvas, VKL_PRIVATE_EVENT_TIMER, 1, _fps, NULL);
 
+    const uint32_t N = 10000;
+    float param = 5.0f;
+    VklGraphicsPointsParams params = {.point_size = param};
+    VklVertex* vertices = calloc(N, sizeof(VklVertex));
+    _add_visual(&scene, N, vertices, &params);
     vkl_app_run(app, 0);
 
     FREE(vertices);
--- cli/test_interact.h
+++ cli/test_interact.h
@@ -12,6 +12,7 @@
 
 int test_interact_1(TestContext* context);
 int test_interact_panzoom(TestContext* context);
+int test_interact_arcball(TestContext* context);
 
 
 #endif
