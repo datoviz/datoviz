Fixing normalization in brain example

bindings/cython/examples/brain.py
@@ -19,12 +19,6 @@ print(f"{N} vertices, {Nf} faces")
 volume, info = mcc.get_template_volume()
 volume *= 100
 
-# Data normalization.
-# TODO: do in Visky instead
-vertices -= vertices.mean(axis=0)
-c = .5 / np.abs(vertices).max()
-vertices *= -c
-
 # Create the scene.
 canvas = canvas()
 panel = canvas.panel(controller='arcball')
@@ -38,9 +32,15 @@ mesh.data('index', indices.astype(np.uint32))
 # Slice plane.
 plane = panel.visual('volume_slice')
 plane.data('pos', np.array([[0, +.25, +.5]], dtype=np.float32), idx=0)
-plane.data('pos', np.array([[0, -.45, -.5]], dtype=np.float32), idx=1)
+plane.data('pos', np.array([[0, +.25, -.5]], dtype=np.float32), idx=1)
+plane.data('pos', np.array([[0, -.45, -.5]], dtype=np.float32), idx=2)
+plane.data('pos', np.array([[0, -.45, +.5]], dtype=np.float32), idx=3)
+
 plane.data('texcoords', np.array([[ 0.5, 0, 0]], dtype=np.float32), idx=0)
-plane.data('texcoords', np.array([[ 0.5, 1, 1]], dtype=np.float32), idx=1)
+plane.data('texcoords', np.array([[ 0.5, 1, 0]], dtype=np.float32), idx=1)
+plane.data('texcoords', np.array([[ 0.5, 1, 1]], dtype=np.float32), idx=2)
+plane.data('texcoords', np.array([[ 0.5, 0, 1]], dtype=np.float32), idx=3)
+
 plane.data('colormap', np.array([[26]], dtype=np.int32))
 plane.data('transferx', np.array([[0, .1, 1, 1]], dtype=np.float32), idx=1)
 plane.data('transfery', np.array([[0, 1, 1, 1]], dtype=np.float32), idx=1)