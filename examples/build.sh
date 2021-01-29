d549862b71bcb1c7f286a093f496dfbb0c073451
Cyrille Rossant <rossant@users.noreply.github.com>
2021-01-29 21:43:44 +0100

    cglm is now a git submodule

--- examples/build.sh
+++ examples/build.sh
@@ -1,2 +1,2 @@
-gcc standalone.c -I../include/ -L../build/ -lvulkan -lm -lvisky -o visky_example
+gcc standalone.c -I../include/ -I../external/cglm/include -L../build/ -lvulkan -lm -lvisky -o visky_example
 ./visky_example
