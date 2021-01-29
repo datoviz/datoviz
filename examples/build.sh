6f06ae606219d42cb03edb56601ab99a9917ca9a
Cyrille Rossant <rossant@users.noreply.github.com>
2021-01-29 17:33:57 +0100

    Add standalone C script

---
+++ examples/build.sh
@@ -0,0 +1,2 @@
+gcc standalone.c -I../include/ -L../build/ -lvulkan -lm -lvisky -o visky_example
+./visky_example
---
+++ examples/standalone.c
@@ -0,0 +1,75 @@
+/*************************************************************************************************/
+/*  Example of a standalone application using the library.                                       */
+/*************************************************************************************************/
+
+/// Import the library public header. The compiler needs to be passed the path to the library
+/// headers.
+#include <visky/visky.h>
+
+int main(int argc, char** argv)
+{
+    // Create a singleton application with a GLFW backend.
+    VklApp* app = vkl_app(VKL_BACKEND_GLFW);
+
+    // Use the first detected GPU. The last argument is the GPU index.
+    VklGpu* gpu = vkl_gpu(app, 0);
+
+    // Create a new canvas with the size specified. The last argument is for optional flags.
+    VklCanvas* canvas = vkl_canvas(gpu, 1280, 1024, 0);
+
+    // Change the canvas clear color.
+    vkl_canvas_clear_color(canvas, (VkClearColorValue){{1, 1, 1, 1}});
+
+    // Create a new scene, which allows to define several subplots (panels) organized within a
+    // grid. Here we just use a single panel spanning the entire canvas.
+    VklScene* scene = vkl_scene(canvas, 1, 1);
+
+    // Get the panel at row 0, column 0, and initialize it with an axes 2D controller.
+    // The last argument is for optional flags.
+    VklPanel* panel = vkl_scene_panel(scene, 0, 0, VKL_CONTROLLER_AXES_2D, 0);
+
+    // Create a new "marker" visual within the panel.
+    // The last argument is for optional flags.
+    VklVisual* visual = vkl_scene_visual(panel, VKL_VISUAL_MARKER, 0);
+
+    // Prepare the data for the marker visual:
+    // - positions (three double-precision floating point numbers for x, y, z)
+    // - color (four bytes for r, g, b, a)
+    // - size (a single precision floating point number with the marker size, in pixels)
+    // There are many other settable visual properties, refer to the visual reference guide.
+    const uint32_t N = 10000;
+    dvec3* pos = (dvec3*)calloc(N, sizeof(dvec3));
+    cvec4* color = (cvec4*)calloc(N, sizeof(cvec4));
+    float* size = (float*)calloc(N, sizeof(float));
+    for (uint32_t i = 0; i < N; i++)
+    {
+        // Random gaussian position.
+        pos[i][0] = randn();
+        pos[i][1] = randn();
+        // Color: random value with a colormap.
+        vkl_colormap_scale(VKL_CMAP_VIRIDIS, rand_float(), 0, 1, color[i]);
+        // A bit of transparency on the alpha channel.
+        color[i][3] = 196;
+        // Random marker size.
+        size[i] = 2 + 30 * rand_float();
+    }
+
+    // We link our data to the visual properties.
+    vkl_visual_data(visual, VKL_PROP_POS, 0, N, pos);
+    vkl_visual_data(visual, VKL_PROP_COLOR, 0, N, color);
+    vkl_visual_data(visual, VKL_PROP_MARKER_SIZE, 0, N, size);
+
+    // We run the application. The last argument is the number of frames to run, or 0 for infinite
+    // loop (stop when escape is pressed or when the window is closed).
+    vkl_app_run(app, 0);
+
+    // We need to clean up all objects handled by Visky at the end.
+    vkl_app_destroy(app);
+
+    // We free the memory of the arrays we've created.
+    FREE(pos);
+    FREE(color);
+    FREE(size);
+
+    return 0;
+}
