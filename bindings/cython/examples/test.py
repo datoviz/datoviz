785b43ec46784bad4c78071c02ef6fad0f2534a9
Cyrille Rossant <rossant@users.noreply.github.com>
2021-01-30 17:25:30 +0100

    Python colormaps

--- bindings/cython/examples/test.py
+++ bindings/cython/examples/test.py
@@ -18,8 +18,11 @@ visual = panel.visual('marker')
 # We set the visual data via "visual properties" (or props). Each visual # comes with a set of
 # predefined props. Here, we set the marker positions, colors (RGBA bytes), and size (in pixels)
 N = 10_000
+color = colormap(nr.rand(N), vmin=0, vmax=1)
+color[:, 3] = 128
+
 visual.data('pos', nr.randn(N, 3))
-visual.data('color', colormap(nr.rand(N), vmin=0, vmax=1))
+visual.data('color', color)
 visual.data('ms', nr.uniform(low=2, high=40, size=N))
 
 # We run the main event loop, which will display the canvas until Escape is # pressed or the
