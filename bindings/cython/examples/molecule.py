737394b518be32c6318fcefa7d9e6873748b56dc
Cyrille Rossant <rossant@users.noreply.github.com>
2021-02-05 12:59:30 +0100

    Add molecule Python example

---
+++ bindings/cython/examples/molecule.py
@@ -0,0 +1,10 @@
+import numpy as np
+import numpy.random as nr
+from datoviz import canvas, run, colormap
+
+c = canvas(show_fps=False)
+panel = c.panel(controller='arcball')
+visual = panel.visual('mesh')
+visual.load_obj("data/mesh/cas9_guide.obj")
+
+run()
