e9ff5c4bca522b2bf524ca279737811ac0bdd9bd
Cyrille Rossant <rossant@users.noreply.github.com>
2021-02-08 19:24:19 +0100

    Add signals Python example

---
+++ bindings/cython/examples/signals.py
@@ -0,0 +1,37 @@
+# Signals.
+
+import numpy as np
+import numpy.random as nr
+
+from datoviz import canvas, run, colormap
+
+c = canvas(show_fps=True)
+
+panel = c.panel(controller='panzoom')
+visual = panel.visual('line_strip')
+
+n_signals = 200
+n_points = 1_000
+n_vert = n_signals * n_points
+
+t = np.linspace(0, 5, n_points)
+x = np.tile(t, (n_signals,))
+assert x.ndim == 1
+
+y = .25 * nr.randn(n_signals, n_points)
+offsets = np.tile(np.arange(n_signals)[:, np.newaxis], (1, n_points))
+y += offsets
+pos = np.c_[x, y.ravel(), np.zeros(n_vert)]
+
+color = np.repeat(colormap(np.linspace(0, 1, n_signals), cmap='winter'), n_points, axis=0)
+length = np.repeat(np.array([n_points]), n_signals)
+
+assert pos.shape == (n_vert, 3)
+assert color.shape == (n_vert, 4)
+assert length.shape == (n_signals,)
+
+visual.data('pos', pos)
+visual.data('color', color)
+visual.data('length', length)
+
+run()
