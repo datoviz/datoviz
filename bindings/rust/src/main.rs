a00e929fdeed878489f217620aaef478c7153f0b
Cyrille Rossant <rossant@users.noreply.github.com>
2021-02-12 18:20:02 +0100

    Update Rust binding demo

--- bindings/rust/src/main.rs
+++ bindings/rust/src/main.rs
@@ -4,16 +4,16 @@ use rand::distributions::{Normal, Distribution};
 
 #[link(name = "datoviz")]
 extern {
-    fn dvz_demo_scatter(point_count: size_t, points: *mut f64);
+    fn dvz_demo_scatter(point_count: i32, points: *mut f64);
 }
 
 fn main() {
 
     let normal = Normal::new(0.0, 0.25);
-    // NOTE: this number is the number of points TIMES 2 (x and y)
-    let mut x: [f64; 50000] = [0.0; 50000];
+    // NOTE: this number is the number of points TIMES 3 (x, y, z)
+    let mut x: [f64; 30000] = [0.0; 30000];
     for i in 0..x.len() {
         x[i] = normal.sample(&mut rand::thread_rng());
     }
-    unsafe { dvz_demo_scatter(25000, x.as_mut_ptr() as *mut f64) };
+    unsafe { dvz_demo_scatter(10000, x.as_mut_ptr() as *mut f64) };
 }
