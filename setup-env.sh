681674fc2f0e3b598ec076f083890afa23cb5436
Cyrille Rossant <rossant@users.noreply.github.com>
2021-02-19 12:03:01 +0100

    Tentative workaround for Anaconda users on Ubuntu (issue #8)

--- setup-env.sh
+++ setup-env.sh
@@ -1,1 +1,4 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/build
+
+# Workaround Anaconda-related issue: https://github.com/datoviz/datoviz/issues/8
+export LD_PRELOAD=/lib/x86_64-linux-gnu/libcairo.so.2
