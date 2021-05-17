3fa4d4fe18529528fa45f86b2c55579fafbce4f5
Cyrille Rossant <rossant@users.noreply.github.com>
2021-05-17 17:59:47 +0200

    Adding Python tests

---
+++ bindings/cython/tests/test_default.py
@@ -0,0 +1,30 @@
+from pytest import fixture
+
+from datoviz import App, app, canvas
+
+
+
+def clear_loggers():
+    """Remove handlers from all loggers"""
+    import logging
+    loggers = [logging.getLogger()] + list(logging.Logger.manager.loggerDict.values())
+    for logger in loggers:
+        handlers = getattr(logger, 'handlers', [])
+        for handler in handlers:
+            logger.removeHandler(handler)
+
+
+def test_app():
+    a = App()
+    a.destroy()
+
+
+
+def test_canvas():
+    c = canvas()
+
+
+def teardown():
+    # HACK: fixes pytest bug
+    # see https://github.com/pytest-dev/pytest/issues/5502#issuecomment-647157873
+    clear_loggers()
