b0f00f32a2ad9a42a7f910a11d249e934856887b
Cyrille Rossant <rossant@users.noreply.github.com>
2021-11-24 15:10:57 +0100

    Window test

--- tests/test_window.c
+++ tests/test_window.c
@@ -23,5 +23,18 @@
 int test_window_1(TstSuite* suite)
 {
     ASSERT(suite != NULL);
+
+    DvzHost* host = dvz_host(DVZ_BACKEND_GLFW);
+    DvzWindow* window = dvz_window(host, 100, 100);
+    AT(window != NULL);
+    AT(window->host != NULL);
+    AT(window->host == host);
+
+    DvzWindow* window2 = dvz_window(host, 100, 100);
+    AT(window2 != NULL);
+    AT(window2->host != NULL);
+    AT(window2->host == host);
+
+    dvz_host_destroy(host);
     return 0;
 }
