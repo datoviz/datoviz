fda5d8442cc6f79a8f7e4fe27d0e8271f67e99a8
Cyrille Rossant <rossant@users.noreply.github.com>
2021-12-03 10:00:30 +0100

    WIP: board functions placeholders

--- include/datoviz/board.h
+++ include/datoviz/board.h
@@ -58,6 +58,66 @@ DVZ_EXPORT DvzBoard dvz_board(DvzGpu* gpu, uint32_t width, uint32_t height);
 
 
 /**
+ * Set the board image format.
+ *
+ * @param board the board
+ * @param format the image format
+ */
+DVZ_EXPORT void dvz_board_format(DvzBoard* board, VkFormat format);
+
+
+/**
+ * Create the board after is has been set up.
+ *
+ * @param board the board
+ */
+DVZ_EXPORT void dvz_board_create(DvzBoard* board);
+
+
+/**
+ * Change the board width and height.
+ *
+ * @param board the board
+ * @param width the width, in framebuffer pixels
+ * @param height the height, in framebuffer pixels
+ */
+DVZ_EXPORT void dvz_board_resize(DvzBoard* board, uint32_t width, uint32_t height);
+
+
+
+/**
+ * Start rendering to the board in a command buffer.
+ *
+ * @param board the board
+ * @param cmds the commands instance
+ * @param idx the command buffer index with the commands instance
+ */
+DVZ_EXPORT void dvz_board_begin(DvzBoard* board, DvzCommands* cmds, uint32_t idx);
+
+
+
+/**
+ * Stop rendering to the board in a command buffer.
+ *
+ * @param board the board
+ * @param cmds the commands instance
+ */
+DVZ_EXPORT void dvz_board_end(DvzBoard* board, DvzCommands* cmds);
+
+
+
+/**
+ * Download the current rendered image.
+ *
+ * @param board the board
+ * @param size the image buffer size (should always be equal to width*height*4)
+ * @param rgba an alread-allocated buffer that will contain the downloaded image
+ */
+DVZ_EXPORT void dvz_board_download(DvzBoard* board, DvzSize size, uint8_t* rgba);
+
+
+
+/**
  * Destroy a board.
  *
  * @param board the board
--- src/board.c
+++ src/board.c
@@ -32,4 +32,52 @@ DvzBoard dvz_board(DvzGpu* gpu, uint32_t width, uint32_t height)
 
 
 
-void dvz_board_destroy(DvzBoard* board) { ASSERT(board != NULL); }
+void dvz_board_format(DvzBoard* board, VkFormat format)
+{
+    ASSERT(board != NULL); //
+}
+
+
+
+void dvz_board_create(DvzBoard* board)
+{
+    ASSERT(board != NULL); //
+}
+
+
+
+void dvz_board_resize(DvzBoard* board, uint32_t width, uint32_t height)
+{
+    ASSERT(board != NULL); //
+}
+
+
+
+void dvz_board_begin(DvzBoard* board, DvzCommands* cmds, uint32_t idx)
+{
+    ASSERT(board != NULL); //
+}
+
+
+
+void dvz_board_end(DvzBoard* board, DvzCommands* cmds)
+{
+    ASSERT(board != NULL);
+    ASSERT(cmds != NULL);
+}
+
+
+
+void dvz_board_download(DvzBoard* board, DvzSize size, uint8_t* rgba)
+{
+    ASSERT(board != NULL);
+    ASSERT(size > 0);
+    ASSERT(rgba != NULL);
+}
+
+
+
+void dvz_board_destroy(DvzBoard* board)
+{
+    ASSERT(board != NULL); //
+}
