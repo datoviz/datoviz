5e2b37b04b964d1b30438433d3d4b33e4fd03328
Cyrille Rossant <rossant@users.noreply.github.com>
2021-12-08 11:53:56 +0100

    WIP: map implementation and tests

--- src/map.cpp
+++ src/map.cpp
@@ -3,6 +3,7 @@
 /*************************************************************************************************/
 
 #include "map.h"
+#include "_log.h"
 
 #include <map>
 #include <numeric>
@@ -16,8 +17,33 @@
 
 extern "C" struct DvzMap
 {
-    std::map<DvzId, void*> _map;
+    std::map<DvzId, std::pair<int, void*>> _map;
+    DvzId last_id;
 };
+
+
+/*************************************************************************************************/
+/*  Utils                                                                                        */
+/*************************************************************************************************/
+
+// https://stackoverflow.com/a/21510202/1595060
+template <typename It> class Range
+{
+    It b, e;
+
+public:
+    Range(It b, It e) : b(b), e(e) {}
+    It begin() const { return b; }
+    It end() const { return e; }
+};
+
+template <
+    typename ORange, typename OIt = decltype(std::begin(std::declval<ORange>())),
+    typename It = std::reverse_iterator<OIt>>
+Range<It> reverse(ORange&& originalRange)
+{
+    return Range<It>(It(std::end(originalRange)), It(std::begin(originalRange)));
+}
 
 
 
@@ -25,11 +51,10 @@ extern "C" struct DvzMap
 /*  Functions                                                                                    */
 /*************************************************************************************************/
 
-
 DvzMap* dvz_map(void)
 {
     DvzMap* map = new DvzMap();
-    map->_map = std::map<DvzId, void*>();
+    map->_map = std::map<DvzId, std::pair<int, void*>>();
     return map;
 }
 
@@ -37,42 +62,60 @@ DvzMap* dvz_map(void)
 
 DvzId dvz_map_id(DvzMap* map)
 {
-    ASSERT(map != NULL); //
+    ASSERT(map != NULL);
+    return ++map->last_id;
 }
 
 
 
 void dvz_map_add(DvzMap* map, DvzId key, int type, void* value)
 {
-    ASSERT(map != NULL); //
+    ASSERT(map != NULL);
+    map->_map[key] = std::pair<int, void*>(type, value);
 }
 
 
 
 void* dvz_map_get(DvzMap* map, DvzId key)
 {
-    ASSERT(map != NULL); //
+    ASSERT(map != NULL);
+    return map->_map[key].second;
 }
 
 
 
 uint32_t dvz_map_count(DvzMap* map, int type)
 {
-    ASSERT(map != NULL); //
+    ASSERT(map != NULL);
+    return map->_map.size();
 }
 
 
 
 void* dvz_map_first(DvzMap* map, int type)
 {
-    ASSERT(map != NULL); //
+    ASSERT(map != NULL);
+    for (const auto& [id, pair] : map->_map)
+    {
+        if (pair.first == type)
+            return pair.second;
+    }
+    log_trace("no item with type %d found in map", type);
+    return NULL;
 }
 
 
 
 void* dvz_map_last(DvzMap* map, int type)
 {
-    ASSERT(map != NULL); //
+    ASSERT(map != NULL);
+    for (const auto& [id, pair] : reverse(map->_map))
+    {
+        if (pair.first == type)
+            return pair.second;
+    }
+    log_trace("no item with type %d found in map", type);
+    return NULL;
 }
 
 
--- tests/test_map.c
+++ tests/test_map.c
@@ -21,4 +21,27 @@
 /*  Map tests                                                                                    */
 /*************************************************************************************************/
 
-int test_map_1(TstSuite* suite) { return 0; }
+int test_map_1(TstSuite* suite)
+{
+
+    DvzMap* map = dvz_map();
+
+    DvzId id = dvz_map_id(map);
+    ASSERT(id == 1);
+
+    int type = 3;
+    int data[2] = {42, 103};
+
+    dvz_map_add(map, id, type, &data);
+    AT(dvz_map_get(map, id) == &data);
+
+    id = dvz_map_id(map);
+    ASSERT(id == 2);
+    dvz_map_add(map, id, type, &data[1]);
+
+    AT(dvz_map_first(map, type) == &data[0]);
+    AT(dvz_map_last(map, type) == &data[1]);
+
+    dvz_map_destroy(map);
+    return 0;
+}
