9b5b9db04bfc6a85e25f16a467335edb2acf8cc5
Cyrille Rossant <rossant@users.noreply.github.com>
2021-12-08 13:51:46 +0100

    Renderer placeholders

--- include/datoviz/renderer.h
+++ include/datoviz/renderer.h
@@ -13,6 +13,11 @@
 
 #include "common.h"
 #include "context.h"
+#include "map.h"
+#include "pipe.h"
+#include "pipelib.h"
+#include "request.h"
+#include "workspace.h"
 
 
 
@@ -32,6 +37,8 @@
 /*  Typedefs                                                                                     */
 /*************************************************************************************************/
 
+typedef struct DvzRenderer DvzRenderer;
+
 // Forward declarations.
 
 
@@ -39,6 +46,16 @@
 /*************************************************************************************************/
 /*  Structs                                                                                      */
 /*************************************************************************************************/
+
+struct DvzRenderer
+{
+    DvzObject obj;
+    DvzGpu* gpu;
+    DvzContext ctx;     // data: the "what"
+    DvzPipelib lib;     // GLSL programs: the "how"
+    DvzWorkspace space; // boards and canvases: the "where"
+    DvzMap* map;        // mapping between uuid and <type, objects>
+};
 
 
 
@@ -49,6 +66,24 @@ EXTERN_C_ON
 /*************************************************************************************************/
 
 // TODO: docstrings
+
+DVZ_EXPORT DvzRenderer dvz_renderer_offscreen(DvzGpu* gpu);
+
+
+
+DVZ_EXPORT DvzRenderer dvz_renderer_glfw(DvzGpu* gpu);
+
+
+
+DVZ_EXPORT DvzId dvz_renderer_request(DvzRenderer* rd, DvzRequest req);
+
+
+
+DVZ_EXPORT void dvz_renderer_image(DvzRenderer* rd, DvzId canvas_id, DvzSize size, uint8_t* rgba);
+
+
+
+DVZ_EXPORT void dvz_renderer_destroy(DvzRenderer* rd);
 
 
 
--- src/renderer.c
+++ src/renderer.c
@@ -21,3 +21,45 @@
 /*************************************************************************************************/
 /*  Functions                                                                                    */
 /*************************************************************************************************/
+
+DvzRenderer dvz_renderer_offscreen(DvzGpu* gpu)
+{
+    ASSERT(gpu != NULL);
+    DvzRenderer rd = {0};
+    rd.gpu = gpu;
+    // DvzContext* ctx = dvz_context(gpu);
+    // ASSERT(ctx != NULL);
+
+    dvz_obj_init(&rd.obj);
+    return rd;
+}
+
+
+
+DvzRenderer dvz_renderer_glfw(DvzGpu* gpu)
+{
+    ASSERT(gpu != NULL); //
+}
+
+
+
+DvzId dvz_renderer_request(DvzRenderer* rd, DvzRequest req)
+{
+    ASSERT(rd != NULL); //
+}
+
+
+
+void dvz_renderer_image(DvzRenderer* rd, DvzId canvas_id, DvzSize size, uint8_t* rgba)
+{
+    ASSERT(rd != NULL);
+}
+
+
+
+void dvz_renderer_destroy(DvzRenderer* rd)
+{
+    ASSERT(rd != NULL);
+
+    dvz_obj_destroyed(&rd->obj);
+}
