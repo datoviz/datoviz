8b5e229c5a42db10a59e5f27f83e09f5c769942d
Cyrille Rossant <rossant@users.noreply.github.com>
2022-02-15 12:53:48 +0100

    Move exprimental code to datoviz/prototypes GitHub repository

--- experimental/rawhttp/run.sh
+++
@@ -1,4 +0,0 @@
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/git/datoviz-distributed/build
-# export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:~/git/datoviz-distributed/build
-export FLASK_APP=serve
-flask run -p 1234
