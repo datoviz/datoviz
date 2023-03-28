f0b7cf62e8eb289ee20ab377e995fd5a5dd91991
Cyrille Rossant <rossant@users.noreply.github.com>
2023-03-28 10:56:32 +0200

    WIP: pixel test

--- src/scene/visuals/pixel.c
+++ src/scene/visuals/pixel.c
@@ -9,7 +9,16 @@
 /*************************************************************************************************/
 
 #include "scene/visuals/pixel.h"
+#include "fileio.h"
 #include "request.h"
+#include "scene/baker.h"
+#include "scene/graphics.h"
+
+
+
+/*************************************************************************************************/
+/*  Macros                                                                                       */
+/*************************************************************************************************/
 
 
 
@@ -26,9 +35,14 @@
 DvzPixel* dvz_pixel(DvzRequester* rqr, int flags)
 {
     ANN(rqr);
+
     DvzPixel* pixel = (DvzPixel*)calloc(1, sizeof(DvzPixel));
     pixel->rqr = rqr;
     pixel->flags = flags;
+
+    pixel->baker = dvz_baker(rqr, 0);
+
+    dvz_obj_init(&pixel->obj);
     return pixel;
 }
 
@@ -37,6 +51,9 @@ DvzPixel* dvz_pixel(DvzRequester* rqr, int flags)
 void dvz_pixel_position(DvzPixel* pixel, uint32_t first, uint32_t count, vec3* values, int flags)
 {
     ANN(pixel);
+    if (!dvz_obj_is_created(&pixel->obj))
+        dvz_pixel_create(pixel);
+    dvz_baker_data(pixel->baker, 0, first, count, (void*)values);
 }
 
 
@@ -44,6 +61,9 @@ void dvz_pixel_position(DvzPixel* pixel, uint32_t first, uint32_t count, vec3* v
 void dvz_pixel_color(DvzPixel* pixel, uint32_t first, uint32_t count, cvec4* values, int flags)
 {
     ANN(pixel);
+    if (!dvz_obj_is_created(&pixel->obj))
+        dvz_pixel_create(pixel);
+    dvz_baker_data(pixel->baker, 1, first, count, (void*)values);
 }
 
 
@@ -51,10 +71,12 @@ void dvz_pixel_color(DvzPixel* pixel, uint32_t first, uint32_t count, cvec4* val
 void dvz_pixel_draw(DvzPixel* pixel, DvzId canvas, uint32_t first, uint32_t count, int flags)
 {
     ANN(pixel);
-    // to be called between dvz_record_begin() and dvz_record_end()
-    // count is the number of *items*
-    // dvz_pixels_update(); // emit the dat update commands
-    // dvz_record_draw(); // emit the record commands
+
+    // Emit the dat update commands.
+    dvz_pixel_update(pixel);
+
+    // Emit the record commands.
+    dvz_record_draw(pixel->rqr, canvas, pixel->graphics_id, first, count, 0, 1);
 }
 
 
@@ -62,22 +84,79 @@ void dvz_pixel_draw(DvzPixel* pixel, DvzId canvas, uint32_t first, uint32_t coun
 void dvz_pixel_create(DvzPixel* pixel)
 {
     ANN(pixel);
+    log_debug("creating pixel visual");
+
+    // NOTE: for now static assignments
+    DvzRequester* rqr = pixel->rqr;
+    ANN(rqr);
+
+    // Create the graphics object.
+    DvzRequest req = dvz_create_graphics(pixel->rqr, DVZ_GRAPHICS_CUSTOM, 0);
+    DvzId graphics_id = req.id;
+    pixel->graphics_id = graphics_id;
+
+    // Load shaders.
+    unsigned long size = 0;
+    unsigned char* buffer = dvz_resource_shader("graphics_basic_vert", &size);
+    dvz_set_spirv(rqr, graphics_id, DVZ_SHADER_VERTEX, size, buffer);
+    buffer = dvz_resource_shader("graphics_basic_frag", &size);
+    dvz_set_spirv(rqr, graphics_id, DVZ_SHADER_VERTEX, size, buffer);
+
+    // Primitive topology.
+    dvz_set_primitive(rqr, graphics_id, DVZ_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST);
+
+    // Polygon mode.
+    dvz_set_polygon(rqr, graphics_id, DVZ_POLYGON_MODE_FILL);
+
+
+    // Set up the baker.
+    DvzBaker* baker = pixel->baker;
+    dvz_baker_vertex(baker, 0, sizeof(DvzPixelVertex));
+    dvz_baker_attr(baker, 0, 0, offsetof(DvzPixelVertex, pos), sizeof(vec3));
+    dvz_baker_attr(baker, 1, 0, offsetof(DvzPixelVertex, color), sizeof(cvec4));
+    dvz_baker_slot(baker, 0, sizeof(DvzMVP));
+    dvz_baker_slot(baker, 1, sizeof(DvzViewport));
+    dvz_baker_duals(baker, 50); // DEBUG
+
+
+    // Vertex binding.
+    dvz_set_vertex(rqr, graphics_id, 0, sizeof(DvzPixelVertex), DVZ_VERTEX_INPUT_RATE_VERTEX);
+
+    // Vertex attrs.
+    dvz_set_attr(
+        rqr, graphics_id, 0, 0, DVZ_FORMAT_R32G32B32_SFLOAT, offsetof(DvzPixelVertex, pos));
+    dvz_set_attr(
+        rqr, graphics_id, 0, 1, DVZ_FORMAT_R8G8B8A8_UNORM, offsetof(DvzPixelVertex, color));
+
+    // Descriptor slots.
+    dvz_set_slot(rqr, graphics_id, 0, DVZ_DESCRIPTOR_TYPE_UNIFORM_BUFFER);
+    dvz_set_slot(rqr, graphics_id, 1, DVZ_DESCRIPTOR_TYPE_UNIFORM_BUFFER);
+
+    dvz_obj_created(&pixel->obj);
+
     /*
+    TODO: automatic attr/slot determination in generic visual
+
     // NOTE: check that props of a given vertex binding idx are either all constant or all not
-    constant dvz_set_primitive(); dvz_set_polygon();
+    constant
 
     // Determine the vertex bindings as a function of the flags.
     // Assume the highest binding_idx of all props +1 is the number of different bindings
     // Array with the stride of each attribute
         uint32_t binding_count;
-        DvzSize strides[DVZ_PROP_MAX_ATTRS]; // for each GLSL attribution, the number of bytes per
-    item DvzSize offsets[DVZ_PROP_MAX_BINDINGS]; // for each binding, the offset of the last
-    visited prop for each prop if (binding_count == 0 || binding >= binding_count) // new binding
-            dvz_set_vertex() // declare new binding
-        dvz_set_attr() // manually called for each prop, with the known GLSL location, and the
-    binding stored in the corresponding DvzProp*
+        DvzSize strides[DVZ_PROP_MAX_ATTRS]; // for each GLSL attr, the number of bytes per
+                                                item
+        DvzSize offsets[DVZ_PROP_MAX_BINDINGS]; // for each binding, the offset of the last
+                                                    visited prop
+        for each prop
+            if (binding_count == 0 || binding >= binding_count) // new binding
+                dvz_set_vertex() // declare new binding
+            dvz_set_attr() // manually called for each prop, with the known GLSL location, and the
+                            binding stored in the corresponding DvzProp*
+
     // Uniform and texture bindings.
     dvz_set_slot()
+
     // Set up the baker
 
     */
@@ -85,12 +164,20 @@ void dvz_pixel_create(DvzPixel* pixel)
 
 
 
-void dvz_pixel_update(DvzPixel* pixel) { ANN(pixel); }
+void dvz_pixel_update(DvzPixel* pixel)
+{
+    ANN(pixel);
+    ANN(pixel->baker);
+    dvz_baker_update(pixel->baker);
+}
 
 
 
 void dvz_pixel_destroy(DvzPixel* pixel)
 {
     ANN(pixel);
+
+    dvz_baker_destroy(pixel->baker);
+
     FREE(pixel);
 }
--- tests/scene/visuals/test_pixel.c
+++ tests/scene/visuals/test_pixel.c
@@ -13,6 +13,7 @@
 #include "scene/visuals/pixel.h"
 #include "test.h"
 #include "testing.h"
+#include "testing_utils.h"
 
 
 
@@ -39,7 +40,84 @@ int test_pixel_1(TstSuite* suite)
 
     DvzPixel* pixel = dvz_pixel(rqr, 0);
 
+    // Upload the data.
+    const uint32_t n = 50;
+
+    // Position.
+    vec3* pos = (vec3*)calloc(n, sizeof(vec3));
+    double t = 0;
+    double aspect = WIDTH / (double)HEIGHT;
+    for (uint32_t i = 0; i < n; i++)
+    {
+        t = i / (double)(n);
+        pos[i][0] = .5 * cos(M_2PI * t);
+        pos[i][1] = aspect * .5 * sin(M_2PI * t);
+    }
+    dvz_pixel_position(pixel, 0, n, pos, 0);
+
+    // Color.
+    cvec4* color = (cvec4*)calloc(n, sizeof(cvec4));
+    for (uint32_t i = 0; i < n; i++)
+    {
+        dvz_colormap(DVZ_CMAP_HSV, TO_BYTE(t), color[i]);
+        color[i][3] = 128;
+    }
+    dvz_pixel_color(pixel, 0, n, color, 0);
+
+
+
+    // DvzGpu* gpu = get_gpu(suite);
+    // ANN(gpu);
+
+    // DvzRenderer* rd = dvz_renderer(gpu, 0);
+
+    // // Create a boards.
+    // DvzRequest req = dvz_create_board(rqr, WIDTH, HEIGHT, DVZ_DEFAULT_CLEAR_COLOR, 0);
+    // dvz_renderer_request(rd, req);
+    // DvzId board_id = req.id;
+
+    // // Board clear color.
+    // req = dvz_set_background(rqr, board_id, (cvec4){32, 64, 128, 255});
+    // dvz_renderer_request(rd, req);
+
+    // // Commands.
+    // dvz_requester_begin(rqr);
+    // dvz_record_begin(rqr, board_id);
+    // dvz_record_viewport(rqr, board_id, DVZ_DEFAULT_VIEWPORT, DVZ_DEFAULT_VIEWPORT);
+
+    // // Draw.
+    // dvz_pixel_draw(pixel, board_id, 0, n, 0);
+
+    // dvz_record_end(rqr, board_id);
+
+    // uint32_t count = 0;
+    // DvzRequest* reqs = dvz_requester_end(rqr, &count);
+    // req = dvz_update_board(rqr, board_id);
+    // dvz_renderer_requests(rd, count, reqs);
+
+    // // Render.
+    // dvz_renderer_request(rd, req);
+
+    // // Retrieve the image.
+    // DvzSize size = 0;
+    // // This pointer will be freed automatically by the renderer.
+    // uint8_t* rgb = dvz_renderer_image(rd, board_id, &size, NULL);
+
+    // // Save to a PNG.
+    // char imgpath[1024];
+    // snprintf(imgpath, sizeof(imgpath), "%s/visual_pixel.png", ARTIFACTS_DIR);
+    // dvz_write_png(imgpath, WIDTH, HEIGHT, rgb);
+    // AT(!dvz_is_empty(WIDTH * HEIGHT * 3, rgb));
+
+    // // Destroy the requester and renderer.
+    // dvz_renderer_destroy(rd);
+
+
+
+    // Cleanup
     dvz_pixel_destroy(pixel);
     dvz_requester_destroy(rqr);
+    FREE(pos);
+    FREE(color);
     return 0;
 }
