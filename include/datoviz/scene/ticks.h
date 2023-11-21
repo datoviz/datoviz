bb97d19ec9d3a23155e3d14ae68d8cbd728e49f0
Cyrille Rossant <rossant@users.noreply.github.com>
2023-11-21 17:33:35 +0100

    WIP: starting ticks implementation

--- include/datoviz/scene/ticks.h
+++ include/datoviz/scene/ticks.h
@@ -33,6 +33,23 @@ typedef struct DvzTicks DvzTicks;
 /*  Enums                                                                                        */
 /*************************************************************************************************/
 
+typedef enum
+{
+    DVZ_TICKS_HORIZONTAL = 0x0000,
+    DVZ_TICKS_VERTICAL = 0x0001,
+    DVZ_TICKS_LOG = 0x0010,
+} DvzTicksFlags;
+
+
+
+// Tick format type.
+typedef enum
+{
+    DVZ_TICKS_FORMAT_UNDEFINED,
+    DVZ_TICKS_FORMAT_DECIMAL,
+    DVZ_TICKS_FORMAT_SCIENTIFIC,
+} DvzTicksFormat;
+
 
 
 /*************************************************************************************************/
@@ -41,7 +58,14 @@ typedef struct DvzTicks DvzTicks;
 
 struct DvzTicks
 {
-    int a;
+    int flags;
+    double range_size; // size in pixels between the two ends of the axis
+    double glyph_size; // average size of a glyph
+    double dmin, dmax; // requested min and max of the range
+
+    double lmin, lmax, lstep; // computed min and max of the ticks
+    DvzTicksFormat format;    // computed tick format
+    uint32_t precision;       // computed tick precision
 };
 
 
@@ -51,6 +75,40 @@ EXTERN_C_ON
 /*************************************************************************************************/
 /*  Functions                                                                                    */
 /*************************************************************************************************/
+
+DVZ_EXPORT DvzTicks* dvz_ticks(int flags);
+
+
+
+DVZ_EXPORT void dvz_ticks_size(DvzTicks* ticks, double range_size, double glyph_size);
+
+
+
+DVZ_EXPORT bool dvz_ticks_compute(DvzTicks* ticks, double dmin, double dmax);
+
+
+
+DVZ_EXPORT double dvz_ticks_score(DvzTicks* ticks);
+
+
+
+DVZ_EXPORT uint32_t dvz_ticks_range(DvzTicks* ticks, dvec3 range);
+
+
+
+DVZ_EXPORT DvzTicksFormat dvz_ticks_format(DvzTicks* ticks);
+
+
+
+DVZ_EXPORT uint32_t dvz_ticks_precision(DvzTicks* ticks);
+
+
+
+DVZ_EXPORT bool dvz_ticks_dirty(DvzTicks* ticks, double dmin, double dmax);
+
+
+
+DVZ_EXPORT void dvz_ticks_destroy(DvzTicks* ticks);
 
 
 
--- src/scene/ticks.c
+++ src/scene/ticks.c
@@ -25,5 +25,83 @@
 
 
 /*************************************************************************************************/
-/*  Ticks  functions                                                                             */
+/*  Ticks functions                                                                              */
 /*************************************************************************************************/
+
+DvzTicks* dvz_ticks(int flags)
+{
+    //
+    return NULL;
+}
+
+
+
+// range_size is in the same unit as glyph size, it's the size between dmin
+// and dmax (below)
+void dvz_ticks_size(DvzTicks* ticks, double range_size, double glyph_size)
+{
+    ANN(ticks);
+    //
+}
+
+
+
+// return if update needed?
+bool dvz_ticks_compute(DvzTicks* ticks, double dmin, double dmax)
+{
+    ANN(ticks);
+    //
+    return false;
+}
+
+
+
+double dvz_ticks_score(DvzTicks* ticks)
+{
+    ANN(ticks);
+    //
+    return 0;
+}
+
+
+
+// lmin, lmax, lstep, returns the number of ticks
+uint32_t dvz_ticks_range(DvzTicks* ticks, dvec3 range)
+{
+    ANN(ticks);
+    return 0;
+}
+
+
+
+DvzTicksFormat dvz_ticks_format(DvzTicks* ticks)
+{
+    // decimal or scientific notation
+    ANN(ticks);
+    return 0;
+}
+
+
+uint32_t dvz_ticks_precision(DvzTicks* ticks)
+{
+    // number of digits after the dot
+    ANN(ticks);
+    return 0;
+}
+
+
+
+// return true if the score with that range is significantly lower than the current score
+bool dvz_ticks_dirty(DvzTicks* ticks, double dmin, double dmax)
+{
+    ANN(ticks);
+    return false;
+}
+
+
+
+void dvz_ticks_destroy(DvzTicks* ticks)
+{
+    ANN(ticks);
+    //
+}
