840a0da6fbdd1be28d5488469d29957d7ea455e0
Cyrille Rossant <rossant@users.noreply.github.com>
2024-03-22 21:51:18 +0100

    Fix bug in visual groups

--- src/scene/visual.c
+++ src/scene/visual.c
@@ -324,6 +324,7 @@ void dvz_visual_groups(DvzVisual* visual, uint32_t group_count, uint32_t* group_
         visual->group_count = group_count;
     }
     ASSERT(visual->group_count >= group_count);
+    visual->group_count = group_count;
 
     // Copy the group sizes.
     memcpy(visual->group_sizes, group_sizes, group_count * sizeof(uint32_t));
