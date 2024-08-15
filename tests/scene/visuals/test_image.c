04dd5c36edcd4478694ac94c515b10b29571def1
Cyrille Rossant <rossant@users.noreply.github.com>
2024-08-15 20:29:29 +0200

    Placeholder in test_image.c with font texture image test

--- tests/scene/visuals/test_image.c
+++ tests/scene/visuals/test_image.c
@@ -8,6 +8,13 @@
 /*  Includes                                                                                     */
 /*************************************************************************************************/
 
+// #include <locale.h>
+// #include <stddef.h>
+// #include <stdint.h>
+// #include <stdio.h>
+// #include <uchar.h>
+// #include "_string.h"
+
 #include "scene/visuals/test_image.h"
 #include "renderer.h"
 #include "request.h"
@@ -30,6 +37,43 @@ int test_image_1(TstSuite* suite)
 {
     VisualTest vt = visual_test_start("image", VISUAL_TEST_PANZOOM, 0);
 
+    // NOTE: quick test with font texture image, need to find a way to define the image size
+    // independently from the viewport size.
+    // // Generate font texture.
+    // uint32_t length = 0;
+    // const char* ptr = "Hello world! 语言处理";
+    // mbstate_t state = {0};
+    // char32_t codepoint;
+    // uint32_t* codepoints = (uint32_t*)calloc(strnlen(ptr, 1024), sizeof(uint32_t));
+    // uint32_t k = 0;
+    // size_t len = 0;
+
+    // while ((len = mbrtoc32(&codepoint, ptr, MB_CUR_MAX, &state)) > 0)
+    // {
+    //     if (len == (size_t)-1 || len == (size_t)-2)
+    //     {
+    //         fprintf(stderr, "Invalid UTF-8 sequence\n");
+    //         return 1;
+    //     }
+
+    //     codepoints[k++] = (uint32_t)codepoint;
+    //     length++;
+    //     ptr += len;
+    // }
+
+    // DvzSize ttf_size = 0;
+    // unsigned char* ttf_bytes = dvz_read_file("data/fonts/Arial-Unicode-Regular.ttf", &ttf_size);
+    // DvzFont* font = dvz_font(ttf_size, ttf_bytes);
+    // dvz_font_size(font, 64);
+    // uvec3 tex_size = {0};
+    // DvzId tex = dvz_font_texture(font, vt.batch, length, codepoints, tex_size);
+    // FREE(codepoints);
+    // dvz_font_destroy(font);
+    // float w = tex_size[0] / (float)WIDTH;
+    // float h = tex_size[1] / (float)HEIGHT;
+
+    float w = 1, h = 1;
+
     // Create the visual.
     DvzVisual* visual = dvz_image(vt.batch, 0);
 
@@ -37,7 +81,7 @@ int test_image_1(TstSuite* suite)
     dvz_image_alloc(visual, 1);
 
     // Image position.
-    dvz_image_position(visual, 0, 1, (vec4[]){{-1, +1, +1, -1}}, 0);
+    dvz_image_position(visual, 0, 1, (vec4[]){{-w, +h, +w, -h}}, 0);
 
     // Image texture coordinates.
     dvz_image_texcoords(visual, 0, 1, (vec4[]){{0, 0, +1, +1}}, 0);
