6a0864b306b714c0e05f58f960ee357de4d16b17
Cyrille Rossant <rossant@users.noreply.github.com>
2025-02-11 22:13:21 +0100

    PyQt6 offscreen server initial backend and example

---
+++ datoviz/backends/pyqt6.py
@@ -0,0 +1,152 @@
+"""PyQt6 backend
+
+"""
+
+# -------------------------------------------------------------------------------------------------
+# Imports
+# -------------------------------------------------------------------------------------------------
+
+import ctypes
+
+from PyQt6.QtWidgets import QLabel, QWidget, QVBoxLayout
+from PyQt6.QtGui import QImage, QPixmap
+from PyQt6.QtCore import Qt
+
+import datoviz as dvz
+from datoviz import vec2
+
+
+# -------------------------------------------------------------------------------------------------
+# Constants
+# -------------------------------------------------------------------------------------------------
+
+RESIZE_TIMER_DELAY = 10  # ms
+WIDTH, HEIGHT = 800, 600
+CHANNEL_COUNT = 3  # assuming RGB888 format
+BUTTON_MAPPING = {
+    Qt.MouseButton.LeftButton: dvz.MOUSE_BUTTON_LEFT,
+    Qt.MouseButton.MiddleButton: dvz.MOUSE_BUTTON_MIDDLE,
+    Qt.MouseButton.RightButton: dvz.MOUSE_BUTTON_RIGHT,
+}
+
+
+# -------------------------------------------------------------------------------------------------
+# Qt figure
+# -------------------------------------------------------------------------------------------------
+
+class QtFigure(QWidget):
+    def __init__(self, figure, server=None, scene=None):
+        super().__init__()
+        self.figure = figure
+        self.server = server
+        self.scene = scene
+        self.mouse = dvz.server_mouse(server)
+
+        self.label = QLabel(self)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+        self.setMinimumSize(50, 50)
+
+        # self.resize_timer = QTimer(self)
+        # self.resize_timer.setSingleShot(True)
+        # self.resize_timer.timeout.connect(self.update_image)
+
+        self.update_image()
+
+    def update_image(self):
+        label_size = self.label.size()
+        w, h = label_size.width(), label_size.height()
+        if w <= 0 or h <= 0:
+            return
+
+        dvz.figure_resize(self.figure, w, h)
+        dvz.figure_update(self.figure)
+        data = dvz.scene_render(self.scene, self.server, self.figure, 0)
+
+        if data is None:
+            return
+
+        data_bytes = bytearray(ctypes.string_at(data, w * h * 3))
+        self.image = QImage(data_bytes, w, h, 3 * w, QImage.Format.Format_RGB888)
+        self.pixmap = QPixmap.fromImage(self.image)
+        self.label.setPixmap(self.pixmap)
+
+    def _mouse_button_pos(self, event):
+        button = event.button()
+        dvz_button = BUTTON_MAPPING.get(button)
+        x = event.position().x()
+        y = event.position().y()
+        return x, y, dvz_button
+
+    def _mouse_move(self, x, y):
+        ev = dvz.mouse_move(self.mouse, vec2(x, y), 0)
+        dvz.scene_mouse(self.scene, self.figure, ev)
+
+    def _mouse_press(self, dvz_button):
+        ev = dvz.mouse_press(self.mouse, dvz_button, 0)
+        dvz.scene_mouse(self.scene, self.figure, ev)
+
+    def _mouse_release(self, dvz_button):
+        ev = dvz.mouse_release(self.mouse, dvz_button, 0)
+        dvz.scene_mouse(self.scene, self.figure, ev)
+
+    def _mouse_wheel(self, dir):
+        ev = dvz.mouse_wheel(self.mouse, vec2(0, dir), 0)
+        dvz.scene_mouse(self.scene, self.figure, ev)
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        # self.resize_timer.start(RESIZE_TIMER_DELAY)
+        self.update_image()
+
+    def mousePressEvent(self, event):
+        super(QtFigure, self).mousePressEvent(event)
+        x, y, dvz_button = self._mouse_button_pos(event)
+        self._mouse_move(x, y)
+        self._mouse_press(dvz_button)
+        self.update_image()
+        event.accept()
+
+    def mouseReleaseEvent(self, event):
+        super(QtFigure, self).mouseReleaseEvent(event)
+        x, y, dvz_button = self._mouse_button_pos(event)
+        self._mouse_move(x, y)
+        self._mouse_release(dvz_button)
+        self.update_image()
+        event.accept()
+
+    def wheelEvent(self, event):
+        super(QtFigure, self).wheelEvent(event)
+        x, y, dvz_button = self._mouse_button_pos(event)
+        delta = event.angleDelta().y()
+        self._mouse_move(x, y)
+        self._mouse_wheel(delta)
+        self.update_image()
+        event.accept()
+
+    def mouseMoveEvent(self, event):
+        super(QtFigure, self).mouseMoveEvent(event)
+        x, y, _ = self._mouse_button_pos(event)
+        self._mouse_move(x, y)
+        self.update_image()
+        event.accept()
+
+
+# -------------------------------------------------------------------------------------------------
+# Qt offscreen server
+# -------------------------------------------------------------------------------------------------
+
+class QtServer:
+    def __init__(self):
+        self.server = dvz.server(0)
+        self.scene = dvz.scene(None)
+
+    def create_figure(self, width=None, height=None):
+        figure = dvz.figure(self.scene, width or WIDTH, height or HEIGHT, 0)
+        return QtFigure(figure, server=self.server, scene=self.scene)
+
+    def __del__(self):
+        dvz.server_destroy(self.server)
--- docs/examples.md
+++ docs/examples.md
@@ -7,6 +7,7 @@
 * [Mesh example](#mesh-example)
 * [Path offscreen example](#path-offscreen-example)
 * [Panels example](#panels-example)
+* [PyQt6 local example](#pyqt6-local-example)
 * [Scatter plot example](#scatter-plot-example)
 * [Spheres example](#spheres-example)
 * [Surface example](#surface-example)
@@ -755,6 +756,78 @@ dvz.scene_run(scene, app, 0)
 # Cleanup.
 dvz.scene_destroy(scene)
 dvz.app_destroy(app)
+```
+</details>
+
+## PyQt6 local example
+
+This example illustrates how to integrate offscreen Datoviz figures into a PyQt6 application,
+using the Datoviz server API which provides a fully offscreen renderer with support for multiple
+canvases.
+
+Illustrates:
+
+- Creating a PyQt6 application
+- Using the Datoviz server API
+- Offscreen rendering with the Datoviz server API
+
+
+
+![](https://raw.githubusercontent.com/datoviz/data/main/screenshots/examples/pyqt_local.png)
+
+<details>
+<summary><strong>👨‍💻 Expand the code</strong> from <code>examples/pyqt_local.py</code></summary>
+
+```python
+import sys
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QSplitter
+from PyQt6.QtCore import Qt
+
+import datoviz as dvz
+from datoviz.backends.pyqt6 import QtServer
+
+
+WIDTH, HEIGHT = 800, 600
+
+
+class ExampleWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Example Qt Datoviz window")
+
+        # Create a Qt Datoviz server.
+        self.qt_server = QtServer()
+
+        # Create two figures (special Qt widgets with a Datoviz figure).
+        w, h = WIDTH // 2, HEIGHT
+        self.qt_figure1 = self.qt_server.create_figure(w, h)
+        self.qt_figure2 = self.qt_server.create_figure(w, h)
+
+        # Fill the figures with mock data.
+        dvz.demo_panel(dvz.panel(self.qt_figure1.figure, 0, 0, w, h))
+        dvz.demo_panel(dvz.panel(self.qt_figure2.figure, 0, 0, w, h))
+
+        # Add the two figures in the main window.
+        splitter = QSplitter(Qt.Orientation.Horizontal)
+        splitter.addWidget(self.qt_figure1)
+        splitter.addWidget(self.qt_figure2)
+        splitter.setCollapsible(0, False)
+        splitter.setCollapsible(1, False)
+        self.setCentralWidget(splitter)
+        self.resize(WIDTH, HEIGHT)
+
+        # self.setMouseTracking(True)
+        # splitter.setMouseTracking(True)
+        # self.qt_figure1.setMouseTracking(True)
+        # self.qt_figure2.setMouseTracking(True)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mw = ExampleWindow()
+    mw.show()
+    sys.exit(app.exec())
 ```
 </details>
 
