eb61bcb46fb1851683aa904815f68ef6d632e54b
Cyrille Rossant <rossant@users.noreply.github.com>
2025-03-04 12:25:12 +0100

    Move GPU from vklite to host

--- include/datoviz/host.h
+++ include/datoviz/host.h
@@ -22,6 +22,7 @@
 #include "_enums.h"
 #include "_obj.h"
 #include "_time_utils.h"
+#include "vklite.h"
 
 
 
@@ -138,6 +139,137 @@ void dvz_host_wait(DvzHost* host);
  * @param host the host to destroy
  */
 int dvz_host_destroy(DvzHost* host);
+
+
+
+/*************************************************************************************************/
+/*  GPU                                                                                          */
+/*************************************************************************************************/
+
+/**
+ * Initialize a GPU.
+ *
+ * A GPU object is the interface to one of the GPUs on the current system.
+ *
+ * @param host the host
+ * @param idx the GPU index among the system's GPUs, or -1 to select the best one
+ * @returns a pointer to the created GPU object
+ */
+DvzGpu* dvz_host_gpu(DvzHost* host, int32_t idx);
+
+
+
+/**
+ * Find the "best" GPU on the system.
+ *
+ * For now, this is just the discrete GPU with the most VRAM, or the GPU with the most VRAM if
+ * there are no discrete GPUs.
+ *
+ * @param host the host
+ * @returns a pointer to the best GPU object
+ */
+DvzGpu* dvz_gpu_best(DvzHost* host);
+
+
+
+/**
+ * Add a new Vulkan device extension.
+ *
+ * @param gpu the GPU
+ * @param extension_name the extension name
+ */
+void dvz_gpu_extension(DvzGpu* gpu, const char* extension_name);
+
+
+
+/**
+ * Specify the export memory handle types.
+ *
+ * @param gpu the GPU
+ * @param flags the flags
+ */
+void dvz_gpu_external(DvzGpu* gpu, VkExternalMemoryHandleTypeFlagsKHR flags);
+
+
+
+/**
+ * Make a renderpass for a GPU.
+ *
+ * Typically, the image layout is VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL for offscreen rendering,
+ * VK_IMAGE_LAYOUT_PRESENT_SRC_KHR for desktop applications.
+ *
+ * @param gpu the GPU
+ * @param clear_color the clear color
+ * @param layout the Vulkan image layout
+ * @returns a renderpass structure
+ */
+DvzRenderpass dvz_gpu_renderpass(DvzGpu* gpu, cvec4 clear_color, VkImageLayout layout);
+
+
+
+/**
+ * Request some features before creating the GPU instance.
+ *
+ * This function needs to be called before creating the GPU with ` dvz_gpu_create()`.
+ *
+ * @param gpu the GPU
+ * @param requested_features the list of requested features
+ */
+void dvz_gpu_request_features(DvzGpu* gpu, VkPhysicalDeviceFeatures requested_features);
+
+
+
+/**
+ * Request a new Vulkan queue before creating the GPU.
+ *
+ * @param gpu the GPU
+ * @param idx the queue index (should be regularly increasing: 0, 1, 2...)
+ * @param type the queue type
+ */
+void dvz_gpu_queue(DvzGpu* gpu, uint32_t idx, DvzQueueType type);
+
+
+
+/**
+ * Create a GPU once the features and queues have been set up.
+ *
+ * @param gpu the GPU
+ * @param surface the surface on which the GPU will need to render
+ */
+void dvz_gpu_create(DvzGpu* gpu, VkSurfaceKHR surface);
+
+
+
+/**
+ * Wait for a queue to be idle.
+ *
+ * This is one of the different GPU synchronization methods. It is not efficient as it waits until
+ * the queue is idle.
+ *
+ * @param gpu the GPU
+ * @param queue_idx the queue index
+ */
+void dvz_queue_wait(DvzGpu* gpu, uint32_t queue_idx);
+
+
+
+/**
+ * Full synchronization on a given GPU.
+ *
+ * This function waits on all queues of a given GPU.
+ *
+ * @param gpu the GPU
+ */
+void dvz_gpu_wait(DvzGpu* gpu);
+
+
+
+/**
+ * Destroy the resources associated to a GPU.
+ *
+ * @param gpu the GPU
+ */
+void dvz_gpu_destroy(DvzGpu* gpu);
 
 
 
