123e61ebe288e0a7efbb601e56c4451dd3657ead
Cyrille Rossant <rossant@users.noreply.github.com>
2025-03-04 11:43:11 +0100

    Add independent host tests file

--- tests/test_vklite.c
+++ tests/test_vklite.c
@@ -83,35 +83,6 @@ static void _save_screenshot(DvzFramebuffers* framebuffers, const char* name)
 /*  Tests                                                                                        */
 /*************************************************************************************************/
 
-int test_host(TstSuite* suite, TstItem* tstitem)
-{
-    ANN(suite);
-    DvzHost* host = dvz_host(DVZ_BACKEND_GLFW);
-    ANN(host);
-    dvz_host_create(host);
-
-    AT(host->obj.status == DVZ_OBJECT_STATUS_CREATED);
-    AT(host->gpus.count >= 1);
-    AT(((DvzGpu*)(host->gpus.items[0]))->name != NULL);
-    AT(((DvzGpu*)(host->gpus.items[0]))->obj.status == DVZ_OBJECT_STATUS_INIT);
-
-    DvzGpu* gpu = dvz_gpu_best(host);
-    dvz_gpu_queue(gpu, 0, DVZ_QUEUE_TRANSFER);
-    dvz_gpu_queue(gpu, 1, DVZ_QUEUE_GRAPHICS | DVZ_QUEUE_COMPUTE);
-    dvz_gpu_queue(gpu, 2, DVZ_QUEUE_COMPUTE);
-    dvz_gpu_create(gpu, 0);
-
-    gpu = dvz_gpu_best(host);
-    ANN(gpu);
-    log_info("Best GPU is %s with %s VRAM", gpu->name, pretty_size(gpu->vram));
-    ANN(gpu->name);
-
-    dvz_host_destroy(host);
-    return 0;
-}
-
-
-
 int test_vklite_commands(TstSuite* suite, TstItem* tstitem)
 {
     ANN(suite);
