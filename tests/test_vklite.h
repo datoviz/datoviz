88412d522a92d78fd4ed81cfd57ffbe1dd42109c
Cyrille Rossant <rossant@users.noreply.github.com>
2025-03-04 11:43:11 +0100

    Add independent host tests file

--- tests/test_vklite.h
+++ tests/test_vklite.h
@@ -25,7 +25,6 @@
 /*  Tests                                                                                        */
 /*************************************************************************************************/
 
-int test_host(TstSuite* suite, TstItem* tstitem);
 int test_vklite_commands(TstSuite* suite, TstItem* tstitem);
 int test_vklite_buffer_1(TstSuite* suite, TstItem* tstitem);
 int test_vklite_buffer_resize(TstSuite* suite, TstItem* tstitem);
