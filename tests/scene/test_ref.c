61148f8552ff3981ca55f2fee65a1fb0c43e4f5f
Cyrille Rossant <rossant@users.noreply.github.com>
2025-03-27 19:50:27 +0100

    Add ref test

--- tests/scene/test_ref.c
+++ tests/scene/test_ref.c
@@ -29,5 +29,64 @@
 int test_ref_1(TstSuite* suite)
 {
     ANN(suite);
+
+    DvzRef* ref = dvz_ref(0);
+
+    DvzDim dim = 0;
+    double vmin = -1;
+    double vmax = +1;
+    uint32_t count = 3;
+
+    dvec2* pos2D = (dvec2*)calloc(count, sizeof(dvec2));
+    dvec3* pos3D = (dvec3*)calloc(count, sizeof(dvec3));
+    vec3* pos_tr = (vec3*)calloc(count, sizeof(vec3));
+
+    dvz_ref_set(ref, dim, vmin, vmax);
+    dvz_ref_expand(ref, dim, 0, 2);
+
+    dvz_ref_get(ref, dim, &vmin, &vmax);
+    AT(vmin == -1);
+    AT(vmax == +2);
+
+
+    // Test expand2D.
+    pos2D[0][0] = -2;
+    pos2D[2][0] = +4;
+
+    dvz_ref_expand2D(ref, count, pos2D);
+    dvz_ref_get(ref, dim, &vmin, &vmax);
+    AT(vmin == -2);
+    AT(vmax == +4);
+
+    // [-2, +4] into [-1, +1]
+    dvz_ref_transform2D(ref, count, pos2D, pos_tr);
+    AC(pos_tr[0][dim], -1, EPS);
+    AC(pos_tr[2][dim], +1, EPS);
+
+
+    // Test expand3D.
+    dim = DVZ_DIM_Z;
+    pos3D[0][dim] = -20;
+    pos3D[1][dim] = +10;
+    pos3D[2][dim] = +40;
+
+    dvz_ref_expand3D(ref, count, pos3D);
+    dvz_ref_get(ref, dim, &vmin, &vmax);
+    AT(vmin == -20);
+    AT(vmax == +40);
+
+    // [-20, +40] into [-1, +1]
+    dvz_ref_transform3D(ref, count, pos3D, pos_tr);
+    AC(pos_tr[0][dim], -1, EPS);
+    AC(pos_tr[1][dim], 0, EPS);
+    AC(pos_tr[2][dim], +1, EPS);
+
+
+    dvz_ref_destroy(ref);
+
+    FREE(pos2D);
+    FREE(pos3D);
+    FREE(pos_tr);
+
     return 0;
 }
