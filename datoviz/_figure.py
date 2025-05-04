ee6e3f6ba449a36591c56fce9dbc52f80eab8d12
Cyrille Rossant <rossant@users.noreply.github.com>
2025-05-04 18:28:46 +0200

    Add docstrings to Figure class

--- datoviz/_figure.py
+++ datoviz/_figure.py
@@ -19,13 +19,45 @@ from ._panel import Panel
 
 
 class Figure:
+    """
+    Represents a figure, which is a container for panels.
+
+    Attributes
+    ----------
+    c_figure : dvz.DvzFigure
+        The underlying C figure object.
+    """
+
     c_figure: dvz.DvzFigure = None
 
-    def __init__(self, c_figure: dvz.DvzFigure):
+    def __init__(self, c_figure: dvz.DvzFigure) -> None:
+        """
+        Initialize a Figure instance.
+
+        Parameters
+        ----------
+        c_figure : dvz.DvzFigure
+            The underlying C figure object.
+        """
         assert c_figure
         self.c_figure = c_figure
 
-    def panel(self, offset: tuple = None, size: tuple = None):
+    def panel(self, offset: tuple[float, float] = None, size: tuple[float, float] = None) -> Panel:
+        """
+        Create a new panel in the figure.
+
+        Parameters
+        ----------
+        offset : tuple of float, optional
+            The (x, y) offset of the panel, in pixels, by default (0, 0).
+        size : tuple of float, optional
+            The (width, height) size of the panel, by default the entire window size.
+
+        Returns
+        -------
+        Panel
+            The created panel instance.
+        """
         if not offset and not size:
             c_panel = dvz.panel_default(self.c_figure)
         else:
@@ -34,8 +66,19 @@ class Figure:
             c_panel = dvz.panel(self.c_figure, x, y, w, h)
         return Panel(c_panel, c_figure=self.c_figure)
 
-    def update(self):
+    def update(self) -> None:
+        """
+        Update the figure.
+        """
         dvz.figure_update(self.c_figure)
 
-    def figure_id(self):
+    def figure_id(self) -> int:
+        """
+        Get the Datoviz internal ID of the figure.
+
+        Returns
+        -------
+        int
+            The ID of the figure.
+        """
         return dvz.figure_id(self.c_figure)
