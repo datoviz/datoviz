cbb783b29978250376bd48bbbc8261b70ce079ef
Cyrille Rossant <rossant@users.noreply.github.com>
2025-05-07 19:01:26 +0200

    Add light feature example

---
+++ examples/features/light.py
@@ -0,0 +1,38 @@
+"""
+# Light example
+
+Show how to manipulate lights.
+
+"""
+
+import datoviz as dvz
+
+app = dvz.App()
+# NOTE: at the moment, you must indicate gui=True if you intend to use a GUI in a figure
+figure = app.figure(gui=True)
+panel = figure.panel()
+visual = panel.demo_3D()
+
+
+light_pos = dvz.vec3(-5, +5, +5)
+light_params = dvz.vec4(0.75, 0.5, 0.75, 0.32)
+
+
+@app.connect(figure)
+def on_gui(ev):
+    dvz.gui_size(dvz.vec2(500, 100))
+    dvz.gui_begin('Change the light', 0)
+    has_changed = False
+    has_changed |= dvz.gui_slider_vec3('light pos', -10, +10, light_pos)
+    has_changed |= dvz.gui_slider_vec4('light params', 0, 1, light_params)
+    dvz.gui_end()
+
+    if has_changed:
+        lp = (light_params[0], light_params[1], light_params[2], light_params[3] * 100)
+        visual.set_light_pos(light_pos)
+        visual.set_light_params(lp)
+        visual.update()
+
+
+app.run()
+app.destroy()
