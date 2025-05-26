66c3e165f6d38621c32b6eb5c996ac7bd42b6f79
Cyrille Rossant <rossant@users.noreply.github.com>
2025-05-26 18:10:28 +0200

    Add marker mode feature example

---
+++ examples/features/marker_mode.py
@@ -0,0 +1,31 @@
+"""
+# Marker rendering modes
+
+Show the different rendering modes of the marker visual.
+
+---
+tags:
+  - marker
+---
+
+"""
+
+import datoviz as dvz
+
+app = dvz.App(background='white')
+figure = app.figure(128, 128)
+panel = figure.panel()
+visual = app.marker(
+    mode='code',
+    shape='asterisk',
+    aspect='outline',
+    position=[[0, 0, 0]],
+    color=[[124, 141, 194, 255]],
+    size=[64],
+    edgecolor=(0, 0, 0, 255),
+    linewidth=2.0,
+)
+panel.add(visual)
+
+app.run()
+app.destroy()
