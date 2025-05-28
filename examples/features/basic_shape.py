b7de40b8af49b41f0961363e073b1eeb20462990
Cyrille Rossant <rossant@users.noreply.github.com>
2025-05-28 14:33:53 +0200

    Add basic shape feature example

---
+++ examples/features/basic_shape.py
@@ -0,0 +1,44 @@
+"""
+# Basic visual from a ShapeCollection
+
+This example demonstrates how to create a basic visual from a `ShapeCollection` in Datoviz.
+
+---
+tags:
+  - shape
+  - arcball
+  - mesh
+in_gallery: true
+make_screenshot: true
+---
+
+"""
+
+import numpy as np
+
+import datoviz as dvz
+
+# Load the bunny mesh.
+sc = dvz.ShapeCollection()
+file_path = dvz.download_data('mesh/bunny.obj')
+sc.add_obj(file_path)
+
+# Initialize the scene.
+app = dvz.App()
+figure = app.figure()
+panel = figure.panel(background=True)
+arcball = panel.arcball(initial=(0.35, 0, 0))
+camera = panel.camera(initial=(0, 0, 3))
+
+# Random colors.
+nv = sc.vertex_count()
+color = np.random.randint(low=0, high=255, size=(nv, 4), dtype=np.uint8)
+color[:, 3] = 255  # Set alpha channel to fully opaque.
+
+# Create a basic visual from the ShapeCollection.
+visual = app.basic(shape=sc, color=color, depth_test=True)
+panel.add(visual)
+
+app.run()
+app.destroy()
+sc.destroy()
