833903ff392e8f8585f528fce5949e3858b513c5
Cyrille Rossant <rossant@users.noreply.github.com>
2025-05-31 11:37:32 +0200

    Add stop feature example

---
+++ examples/features/stop.py
@@ -0,0 +1,30 @@
+"""
+# Stopping an application
+
+Show how to stop the application while it is running.
+
+---
+tags:
+  - stop
+in_gallery: true
+make_screenshot: false
+---
+
+"""
+
+import datoviz as dvz
+
+app = dvz.App()
+figure = app.figure()
+panel = figure.panel(background=True)
+panel.demo_2D()
+
+
+# This call will stop the application after 500 ms.
+@app.timer(delay=0.5)
+def on_timer(ev):
+    app.stop()
+
+
+app.run()
+app.destroy()
