524b061042799b96d6ec3f785026bf18202154c3
Cyrille Rossant <rossant@users.noreply.github.com>
2025-06-12 23:16:54 +0200

    Fix cleanup in visual test code

--- tests/scene/visuals/visual_test.h
+++ tests/scene/visuals/visual_test.h
@@ -175,16 +175,10 @@ static void visual_test_end(VisualTest vt)
 
     // Run the scene.
     dvz_scene_run(vt.scene, vt.app, N_FRAMES);
-    dvz_app_destroy(vt.app);
 
     // Cleanup.
-    if (vt.camera != NULL)
-        dvz_camera_destroy(vt.camera);
-    if (vt.visual != NULL)
-        dvz_visual_destroy(vt.visual);
-    dvz_panel_destroy(vt.panel);
-    dvz_figure_destroy(vt.figure);
     dvz_scene_destroy(vt.scene);
+    dvz_app_destroy(vt.app);
 }
 
 
