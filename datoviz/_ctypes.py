70721c2c48ff0e3fc3efad10cb821538e0968f63
Cyrille Rossant <rossant@users.noreply.github.com>
2025-07-07 18:33:08 +0200

    Bump to development version

--- datoviz/_ctypes.py
+++ datoviz/_ctypes.py
@@ -6,7 +6,7 @@ SPDX-License-Identifier: MIT
 
 # WARNING: DO NOT EDIT: automatically-generated file
 
-__version__ = "0.3.1"
+__version__ = "0.3.2-dev"
 
 
 # ===============================================================================
