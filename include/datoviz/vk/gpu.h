234fd2b140200b7f599667e6f215ced75fe63d0e
Cyrille Rossant <rossant@users.noreply.github.com>
2025-10-23 17:13:49 +0200

    WIP: GPU function signatures

--- include/datoviz/vk/gpu.h
+++ include/datoviz/vk/gpu.h
@@ -41,7 +41,7 @@ typedef struct DvzDevice DvzDevice;
 
 
 /*************************************************************************************************/
-/*  Functions                                                                                    */
+/*  GPU                                                                                          */
 /*************************************************************************************************/
 
 /**
@@ -56,9 +56,24 @@ DVZ_EXPORT DvzGpu* dvz_instance_gpus(DvzInstance* instance, uint32_t* count);
 
 
 /**
+ * Create a logical device from a physical GPU device.
+ *
+ * @param gpu the GPU
+ * @param[out] device the created logical device
+ * @returns the creation result
+ */
+DVZ_EXPORT int dvz_gpu_device(DvzGpu* gpu, DvzDevice* device);
+
+
+
+/*************************************************************************************************/
+/*  GPU properties                                                                               */
+/*************************************************************************************************/
+
+/**
  * Return physical device properties for Vulkan 1.0.
  *
- * @param gpu
+ * @param gpu the GPU
  * @returns properties for Vulkan 1.0
  */
 DVZ_EXPORT VkPhysicalDeviceProperties dvz_gpu_properties10(DvzGpu* gpu);
@@ -68,7 +83,7 @@ DVZ_EXPORT VkPhysicalDeviceProperties dvz_gpu_properties10(DvzGpu* gpu);
 /**
  * Return physical device properties for Vulkan 1.1.
  *
- * @param gpu
+ * @param gpu the GPU
  * @returns properties for Vulkan 1.1
  */
 DVZ_EXPORT VkPhysicalDeviceVulkan11Properties dvz_gpu_properties11(DvzGpu* gpu);
@@ -78,7 +93,7 @@ DVZ_EXPORT VkPhysicalDeviceVulkan11Properties dvz_gpu_properties11(DvzGpu* gpu);
 /**
  * Return physical device properties for Vulkan 1.2.
  *
- * @param gpu
+ * @param gpu the GPU
  * @returns properties for Vulkan 1.2
  */
 DVZ_EXPORT VkPhysicalDeviceVulkan12Properties dvz_gpu_properties12(DvzGpu* gpu);
@@ -88,7 +103,7 @@ DVZ_EXPORT VkPhysicalDeviceVulkan12Properties dvz_gpu_properties12(DvzGpu* gpu);
 /**
  * Return physical device properties for Vulkan 1.3.
  *
- * @param gpu
+ * @param gpu the GPU
  * @returns properties for Vulkan 1.3
  */
 DVZ_EXPORT VkPhysicalDeviceVulkan13Properties dvz_gpu_properties13(DvzGpu* gpu);
@@ -98,43 +113,78 @@ DVZ_EXPORT VkPhysicalDeviceVulkan13Properties dvz_gpu_properties13(DvzGpu* gpu);
 /**
  * Return physical device memory properties.
  *
- * @param gpu
+ * @param gpu the GPU
  * @returns memory properties
  */
 DVZ_EXPORT VkPhysicalDeviceMemoryProperties dvz_gpu_memprops(DvzGpu* gpu);
 
 
 
-DVZ_EXPORT char** dvz_gpu_supported_extensions(DvzGpu* gpu, uint32_t* count);
+/*************************************************************************************************/
+/*  Device features                                                                              */
+/*************************************************************************************************/
+
+/**
+ * Return physical device features for Vulkan 1.0.
+ *
+ * @param gpu the GPU
+ * @returns features for Vulkan 1.0
+ */
+DVZ_EXPORT VkPhysicalDeviceFeatures dvz_gpu_features10(DvzGpu* gpu);
 
 
 
-DVZ_EXPORT bool dvz_gpu_has_extension(DvzGpu* gpu, const char* extension);
+/**
+ * Return physical device features for Vulkan 1.1.
+ *
+ * @param gpu the GPU
+ * @returns features for Vulkan 1.1
+ */
+DVZ_EXPORT VkPhysicalDeviceVulkan11Features dvz_gpu_features11(DvzGpu* gpu);
 
 
 
-DVZ_EXPORT void dvz_gpu_extensions(DvzGpu* gpu, uint32_t count, const char** extensions);
+/**
+ * Return physical device features for Vulkan 1.2.
+ *
+ * @param gpu the GPU
+ * @returns features for Vulkan 1.2
+ */
+DVZ_EXPORT VkPhysicalDeviceVulkan12Features dvz_gpu_features12(DvzGpu* gpu);
 
 
 
-DVZ_EXPORT void dvz_gpu_extension(DvzGpu* gpu, const char* extension);
+/**
+ * Return physical device features for Vulkan 1.3.
+ *
+ * @param gpu the GPU
+ * @returns features for Vulkan 1.3
+ */
+DVZ_EXPORT VkPhysicalDeviceVulkan13Features dvz_gpu_features13(DvzGpu* gpu);
 
 
 
-DVZ_EXPORT char** dvz_gpu_supported_features(DvzGpu* gpu, uint32_t* count);
+/*************************************************************************************************/
+/*  Device extensions                                                                            */
+/*************************************************************************************************/
+
+/**
+ * Get the supported extensions before creating a GPU.
+ *
+ * !!! warning
+ *     The caller needs to free the returned array AS WELL AS every string in it.
+ *
+ * @param[out] count the number of supported extensions
+ * @returns a pointer to an array of strings
+ */
+DVZ_EXPORT char** dvz_gpu_supported_extensions(uint32_t* count);
 
 
 
-DVZ_EXPORT bool dvz_gpu_has_feature(DvzGpu* gpu, const char* feature);
-
-
-
-DVZ_EXPORT void dvz_gpu_features(DvzGpu* gpu, uint32_t count, const char** features);
-
-
-
-DVZ_EXPORT void dvz_gpu_feature(DvzGpu* gpu, const char* feature);
-
-
-
-DVZ_EXPORT int dvz_gpu_device(DvzGpu* gpu, DvzDevice* device);
+/**
+ * Returns whether an GPU extension is supported?
+ *
+ * @param extension the extension name
+ * @returns a boolean indicating whether this extension is supported by the GPU
+ */
+DVZ_EXPORT bool dvz_gpu_has_extension(const char* extension);
