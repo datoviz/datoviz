801d18e7f72c77090808c46ed6f6fac2057dc7ac
Cyrille Rossant <rossant@users.noreply.github.com>
2025-11-04 17:10:31 +0100

    WIP: graphics test with rendering to an image

--- src/vklite/tests/test_graphics.c
+++ src/vklite/tests/test_graphics.c
@@ -32,6 +32,7 @@
 #include "datoviz/vklite/commands.h"
 #include "datoviz/vklite/graphics.h"
 #include "datoviz/vklite/images.h"
+#include "datoviz/vklite/rendering.h"
 #include "datoviz/vklite/shader.h"
 #include "datoviz/vklite/slots.h"
 #include "test_vklite.h"
@@ -41,7 +42,16 @@
 
 
 /*************************************************************************************************/
-/*  Graphics tests                                                                            */
+/*  Constants                                                                                    */
+/*************************************************************************************************/
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+
+
+/*************************************************************************************************/
+/*  Graphics tests                                                                               */
 /*************************************************************************************************/
 
 int test_vklite_graphics_1(TstSuite* suite, TstItem* tstitem)
@@ -102,16 +112,44 @@ int test_vklite_graphics_1(TstSuite* suite, TstItem* tstitem)
         &graphics, VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST, DVZ_GRAPHICS_FLAGS_FIXED);
 
     // Dynamic state.
-    dvz_graphics_viewport(&graphics, 0, 0, 800, 600, 0, 1, DVZ_GRAPHICS_FLAGS_DYNAMIC);
+    dvz_graphics_viewport(&graphics, 0, 0, WIDTH, HEIGHT, 0, 1, DVZ_GRAPHICS_FLAGS_DYNAMIC);
 
     // Graphics creation.
     dvz_graphics_create(&graphics);
+
+    // Rendering.
+    DvzRendering rendering = {0};
+    dvz_rendering(&rendering);
+    dvz_rendering_area(&rendering, 0, 0, WIDTH, HEIGHT);
+
+    // Image to render to.
+    VkImageLayout img_layout = VK_IMAGE_LAYOUT_ATTACHMENT_OPTIMAL;
+
+    DvzImages images = {0};
+    dvz_images(&bootstrap.device, &bootstrap.allocator, VK_IMAGE_TYPE_2D, 1, &images);
+    dvz_images_format(&images, VK_FORMAT_R8G8B8A8_UNORM);
+    dvz_images_size(&images, WIDTH, HEIGHT, 1);
+    dvz_images_usage(&images, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT);
+    dvz_images_layout(&images, img_layout);
+    dvz_images_create(&images);
+
+    // Image views.
+    DvzImageViews view = {0};
+    dvz_image_views(&images, &view);
+    dvz_image_views_create(&view);
+
+    // Attachments.
+    DvzAttachment* attachment = dvz_rendering_color(&rendering, 0);
+    dvz_attachment_image(attachment, dvz_image_views_handle(&view, 0), img_layout);
+    dvz_attachment_ops(attachment, VK_ATTACHMENT_LOAD_OP_CLEAR, VK_ATTACHMENT_STORE_OP_STORE);
 
     // Command buffer.
     DvzCommands cmds = {0};
     dvz_commands(device, queue, 1, &cmds);
     dvz_cmd_begin(&cmds, 0);
+    dvz_cmd_rendering_begin(&cmds, 0, &rendering);
     dvz_cmd_bind_graphics(&cmds, 0, &graphics);
+    dvz_cmd_rendering_end(&cmds, 0);
     dvz_cmd_end(&cmds, 0);
 
     // Cleanup.
