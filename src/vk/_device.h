708c9ace9a04cfb58129d5855f3062a0c7b08ebe
Cyrille Rossant <rossant@users.noreply.github.com>
2026-02-13 15:33:16 -0800

    Add missing internal files during refactoring

---
+++ src/vk/_device.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2021 Cyrille Rossant and contributors. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ * SPDX-License-Identifier: MIT
+ */
+
+/*************************************************************************************************/
+/*  Internal device                                                                              */
+/*************************************************************************************************/
+
+#pragma once
+
+
+
+/*************************************************************************************************/
+/*  Includes                                                                                     */
+/*************************************************************************************************/
+
+#include "datoviz/common/obj.h"
+#include "datoviz/vk/device.h"
+
+
+
+/*************************************************************************************************/
+/*  Structs                                                                                      */
+/*************************************************************************************************/
+
+struct DvzDevice
+{
+    DvzObject obj;
+    bool is_heap_allocated;
+    DvzGpu* gpu;
+
+    DvzQueues queues;
+    VkDeviceCreateInfo info;
+
+    uint32_t req_extension_count;
+    char* req_extensions[DVZ_MAX_REQ_EXTENSIONS];
+
+    VkPhysicalDeviceFeatures2 features;
+    VkPhysicalDeviceVulkan11Features features11;
+    VkPhysicalDeviceVulkan12Features features12;
+    VkPhysicalDeviceVulkan13Features features13;
+
+    VkDevice vk_device;
+    VkCommandPool cpools[DVZ_MAX_QUEUE_FAMILIES];
+    VkDescriptorPool dpool;
+};
+
+
+
+/*************************************************************************************************/
+/*  Internal device API                                                                          */
+/*************************************************************************************************/
+
+void dvz_gpu_device(DvzGpu* gpu, DvzDevice* device);
+void dvz_device_request_queues(DvzDevice* device, uint32_t family, uint32_t count);
+int dvz_device_build(DvzDevice* device);
+void dvz_device_request_canvas_extensions(DvzDevice* device);
+bool dvz_device_request_extension(DvzDevice* device, const char* extension);
+VkPhysicalDeviceFeatures* dvz_device_request_features10(DvzDevice* device);
+VkPhysicalDeviceVulkan11Features* dvz_device_request_features11(DvzDevice* device);
+VkPhysicalDeviceVulkan12Features* dvz_device_request_features12(DvzDevice* device);
+VkPhysicalDeviceVulkan13Features* dvz_device_request_features13(DvzDevice* device);
---
+++ src/vk/_instance.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2021 Cyrille Rossant and contributors. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ * SPDX-License-Identifier: MIT
+ */
+
+/*************************************************************************************************/
+/*  Internal instance                                                                            */
+/*************************************************************************************************/
+
+#pragma once
+
+
+
+/*************************************************************************************************/
+/*  Includes                                                                                     */
+/*************************************************************************************************/
+
+#include "datoviz/common/obj.h"
+#include "datoviz/vk/gpu.h"
+#include "datoviz/vk/instance.h"
+
+
+
+/*************************************************************************************************/
+/*  Structs                                                                                      */
+/*************************************************************************************************/
+
+struct DvzInstance
+{
+    DvzObject obj;
+    bool is_heap_allocated;
+
+    VkInstance vk_instance;
+    uint32_t vk_version;
+
+    // Instance creation structures.
+    bool flags;
+    bool portability;
+    VkInstanceCreateInfo info_inst;
+    VkDebugUtilsMessengerCreateInfoEXT info_debug;
+    VkValidationFeaturesEXT validation_features;
+
+    // Requested layers and extensions.
+    uint32_t req_layer_count;
+    uint32_t req_extension_count;
+    char* req_layers[DVZ_MAX_REQ_LAYERS];
+    char* req_extensions[DVZ_MAX_REQ_EXTENSIONS];
+
+    // All supported instance layers and extensions.
+    uint32_t layer_count;
+    uint32_t extension_count;
+    char** layers;
+    char** extensions;
+
+    // Application info.
+    char* name;
+    uint32_t version;
+
+    // Validation.
+    VkDebugUtilsMessengerEXT debug_messenger;
+    uint32_t n_errors;
+
+    // GPUs.
+    uint32_t gpu_count;
+    DvzGpu gpus[DVZ_MAX_GPUS];
+};
+
+
+
+/*************************************************************************************************/
+/*  Internal instance API                                                                        */
+/*************************************************************************************************/
+
+void dvz_instance(DvzInstance* instance, int flags);
+void dvz_instance_info(DvzInstance* instance, const char* name, uint32_t version);
+void dvz_instance_portability(DvzInstance* instance);
+void dvz_instance_validation_pre(DvzInstance* instance);
+void dvz_instance_validation_post(DvzInstance* instance);
+int dvz_instance_build(DvzInstance* instance, uint32_t vk_version);
+void dvz_instance_request_layer(DvzInstance* instance, const char* layer);
+void dvz_instance_request_extension(DvzInstance* instance, const char* extension);
