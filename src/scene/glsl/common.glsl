7dde9f1cdd7077c839311f8d7b6bf344e6462b20
Cyrille Rossant <rossant@users.noreply.github.com>
2026-05-07 20:09:04 +0200

    scene: extract shader transform() into common.glsl

---
+++ src/scene/glsl/common.glsl
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021 Cyrille Rossant and contributors. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ * SPDX-License-Identifier: MIT
+ */
+
+/* Shared transform pipeline for builtin scene vertex shaders.
+ *
+ * Bind group layout (set 0):
+ *   binding 0: MVP { model, view, proj, time, flags }
+ *
+ * Single entry point: transform(pos) returns a Vulkan-correct gl_Position.
+ *  - Applies the MVP chain (cglm-built, OpenGL-NDC convention).
+ *  - Converts OpenGL NDC to Vulkan NDC (Y down, depth [0,1]).
+ */
+
+#ifndef DVZ_COMMON_GLSL
+#define DVZ_COMMON_GLSL
+
+layout(set = 0, binding = 0) uniform MVP {
+    mat4 model;
+    mat4 view;
+    mat4 proj;
+    float time;
+    uint flags;
+} mvp;
+
+vec4 transform(vec3 pos)
+{
+    vec4 tr = mvp.proj * mvp.view * mvp.model * vec4(pos, 1.0);
+    tr.y = -tr.y;
+    tr.z = 0.5 * (tr.z + tr.w);
+    return tr;
+}
+
+#endif
--- src/scene/glsl/point.vert
+++ src/scene/glsl/point.vert
@@ -1,12 +1,6 @@
 #version 450
 
-layout(set = 0, binding = 0) uniform MVP {
-    mat4 model;
-    mat4 view;
-    mat4 proj;
-    float time;
-    uint flags;
-} mvp;
+#include "common.glsl"
 
 layout(location = 0) in vec3 inPos;
 layout(location = 1) in vec4 inColor;
@@ -15,11 +9,7 @@ layout(location = 2) in float inSize;
 layout(location = 0) out vec4 fragColor;
 
 void main() {
-    vec4 tr = mvp.proj * mvp.view * mvp.model * vec4(inPos, 1.0);
-    /* OpenGL -> Vulkan NDC: flip Y, remap depth from [-1,1] to [0,1]. */
-    tr.y = -tr.y;
-    tr.z = 0.5 * (tr.z + tr.w);
-    gl_Position = tr;
+    gl_Position = transform(inPos);
     gl_PointSize = inSize;
     fragColor = inColor;
 }
