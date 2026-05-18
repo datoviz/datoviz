82f1a86921adf31adf31ba570f9bba2b029ada50
Cyrille Rossant <rossant@users.noreply.github.com>
2026-05-18 20:49:28 +0200

    Add independent CPU test runners

---
+++ testing/components/dvztest_common.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2021 Cyrille Rossant and contributors. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ * SPDX-License-Identifier: MIT
+ */
+
+/*************************************************************************************************/
+/*  Datoviz common test runner                                                                   */
+/*************************************************************************************************/
+
+
+
+/*************************************************************************************************/
+/*  Includes                                                                                     */
+/*************************************************************************************************/
+
+#include "_log.h"
+#include "../../src/common/tests/test_common.h"
+#include "datoviz_testing.h"
+#include "testing.h"
+
+
+
+/*************************************************************************************************/
+/*  Entry-point                                                                                  */
+/*************************************************************************************************/
+
+/**
+ * Run common-module tests.
+ *
+ * @param argc command-line argument count
+ * @param argv command-line arguments
+ * @return process exit code
+ */
+int main(int argc, char** argv)
+{
+    log_set_level_env();
+
+    TstSuite suite = tst_suite();
+    dvz_testing_install_log_adapter(&suite);
+
+    test_common(&suite);
+
+    int res = tst_suite_run(&suite, argc, argv);
+    tst_suite_destroy(&suite);
+    return res;
+}
---
+++ testing/components/dvztest_ds.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2021 Cyrille Rossant and contributors. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ * SPDX-License-Identifier: MIT
+ */
+
+/*************************************************************************************************/
+/*  Datoviz data-structure test runner                                                           */
+/*************************************************************************************************/
+
+
+
+/*************************************************************************************************/
+/*  Includes                                                                                     */
+/*************************************************************************************************/
+
+#include "_log.h"
+#include "../../src/ds/tests/test_ds.h"
+#include "datoviz_testing.h"
+#include "testing.h"
+
+
+
+/*************************************************************************************************/
+/*  Entry-point                                                                                  */
+/*************************************************************************************************/
+
+/**
+ * Run data-structure module tests.
+ *
+ * @param argc command-line argument count
+ * @param argv command-line arguments
+ * @return process exit code
+ */
+int main(int argc, char** argv)
+{
+    log_set_level_env();
+
+    TstSuite suite = tst_suite();
+    dvz_testing_install_log_adapter(&suite);
+
+    test_ds(&suite);
+
+    int res = tst_suite_run(&suite, argc, argv);
+    tst_suite_destroy(&suite);
+    return res;
+}
