a2e01255bf2e71e8912053e0a844112352ce6353
Cyrille Rossant <rossant@users.noreply.github.com>
2026-06-17 13:01:58 +0200

    packaging: install vcpkg usage text

--- vcpkg-overlay/ports/datoviz/portfile.cmake
+++ vcpkg-overlay/ports/datoviz/portfile.cmake
@@ -34,6 +34,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME datoviz CONFIG_PATH lib/cmake/datoviz)
 vcpkg_fixup_pkgconfig()
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
